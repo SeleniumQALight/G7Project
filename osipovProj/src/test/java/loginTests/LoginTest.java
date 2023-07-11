ba0f30d584377e5faae1d76d56a235bcbedc2a76
ivan-o-github <127018501+ivan-o-github@users.noreply.github.com>
2023-07-11 21:59:11 +0300

    11/07 classwork LoginTest

---
+++ osipovProj/src/test/java/loginTests/LoginTest.java
@@ -0,0 +1,45 @@
+package loginTests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+    WebDriver webDriver;
+    @Test
+    public void validLoginIn(){
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        System.out.println("Browser was opened");
+
+        webDriver.get("https://qa-complexapp.onrender.com/");
+        System.out.println("site was opened");
+
+        WebElement inputUserName = webDriver.findElement(By.xpath("//input[@placeholder='Username']"));
+        inputUserName.clear();
+        inputUserName.sendKeys("qaauto");
+        System.out.println("UserName was inputted");
+
+        WebElement inputPassword = webDriver.findElement(By.xpath("//input[@placeholder='Password']"));
+        inputPassword.clear();
+        inputPassword.sendKeys("123456qwerty");
+        System.out.println("Password was inputted");
+
+        webDriver.findElement(By.xpath(".//button[@class='btn btn-primary btn-sm']")).click();
+        System.out.println("Button was clicked");
+
+        WebElement buttonSignOut = webDriver.findElement(By.xpath(".//button[@class='btn btn-primary btn-sm']"));
+        Assert.assertTrue("Button is not displayed", buttonSignOut.isDisplayed());
+
+        webDriver.quit();
+        System.out.println("Browser was closed");
+    }
+}
