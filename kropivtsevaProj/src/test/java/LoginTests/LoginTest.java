301cb09e87b15f20853311f10b8f114be045edf8
Kropivtseva_Daria <kropivtseva.daria@gmail.com>
2023-07-17 18:22:07 +0300

    Create test InvalidLoginIn

--- kropivtsevaProj/src/test/java/LoginTests/LoginTest.java
+++ kropivtsevaProj/src/test/java/LoginTests/LoginTest.java
@@ -59,4 +59,46 @@ public class LoginTest {
             return false;
         }
     }
+
+    @Test
+
+    public void invalidLoginIn() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        System.out.println("Browser was opened");
+
+        webDriver.get("https://qa-complexapp.onrender.com/");
+        System.out.println("site was opened");
+
+        WebElement inputUserName = webDriver.findElement(By.xpath("//input[@placeholder='Username']"));
+        inputUserName.clear();
+        inputUserName.sendKeys("dariak");
+        System.out.println("User name was inputted");
+
+        WebElement inputUserPassword = webDriver.findElement(By.xpath("//input[@placeholder='Password']"));
+        inputUserPassword.clear();
+        inputUserPassword.sendKeys("123456qwert");
+
+        webDriver.findElement(By.xpath(".//button[text()='Sign In']")).click();
+        System.out.println("Button was clicked");
+
+        Assert.assertTrue("Message - Invalid username/password.", isMessageInvalidUsernamePasswordVisible());
+
+    }
+
+    @After
+    public void tearDownInvalidLoginIn() {
+        webDriver.quit();
+        System.out.println("Browser was closed");
+    }
+
+    private boolean isMessageInvalidUsernamePasswordVisible() {
+        try {
+            return webDriver.findElement(By.xpath(".//div[@class=\'alert alert-danger text-center\' and text()=\'Invalid username/password.\']")).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
