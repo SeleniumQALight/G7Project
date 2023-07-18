bc5759d92a60b3ba2c79af602ded1d36814643a8
Natali <klets.natali@gmail.com>
2023-07-18 11:41:25 -0700

    LoginTestWithPageObject

--- kletsProj/src/main/java/pages/ActionsWithElements.java
+++ kletsProj/src/main/java/pages/ActionsWithElements.java
@@ -3,12 +3,15 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class ActionsWithElements {
     WebDriver webDriver;
 
     public ActionsWithElements(WebDriver webDriver) {
         this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this); // this - means all elements from this class will be initialized
+        // element in Find by
     }
 
     public void openPage(String url) {
@@ -46,6 +49,10 @@ public class ActionsWithElements {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    public void checkElementDisplayed(WebElement element) {
+        Assert.assertTrue("Element is not displayed", isElementDisplayed(element));
     }
 
     private void printErrorAndStopTest(Exception e) {
