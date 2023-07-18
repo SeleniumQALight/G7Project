a54f5de419c40dd49d7c86c65bd51a4bc11d6743
olehnalbatov <oleh.nalbatov@htmi.ch>
2023-07-18 14:23:08 +0300

    InvalidLoginFinal

--- nalbatovProj/src/test/java/loginTests/LoginTests.java
+++ nalbatovProj/src/test/java/loginTests/LoginTests.java
@@ -41,11 +41,9 @@ public class LoginTests {
         System.out.println("Button was clicked");
 
         // WebElement buttonSignOut = webDriver.findElement(By.xpath(".//button[text()='Sign Out']"));
-        Assert.assertTrue("Sign in button is not displayed", isButtonSignInVisible());
-        System.out.println("Sign in button is displayed");
+
         Assert.assertTrue("button is not displayed", isButtonSignOutVisible());
-        Assert.assertTrue("Error message is not displayed", isInvalidDataMessegeVisible());
-        System.out.println("Error message is displayed");
+
 
 
         //webDriver.quit();
@@ -56,6 +54,39 @@ public class LoginTests {
         webDriver.quit();
         System.out.println("Browser was closed");
     }
+    @Test
+    public void invalidLoginIn() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        System.out.println("Browser was opened");
+
+        webDriver.get("https://qa-complexapp.onrender.com");
+        System.out.println("Site was opened");
+
+        WebElement inputUserName = webDriver.findElement(By.xpath("//input[@placeholder='Username']"));
+        inputUserName.clear();
+        inputUserName.sendKeys("qaauto");
+        System.out.println("Username was inputted");
+
+        WebElement inputPassword = webDriver.findElement(By.xpath("//input[@placeholder='Password']"));
+        inputPassword.clear();
+        inputPassword.sendKeys("123456qwert");
+        System.out.println(" Invalid password was inputted");
+
+        webDriver.findElement(By.xpath(".//button[@class='btn btn-primary btn-sm']")).click();
+        System.out.println("Button was clicked");
+
+        Assert.assertTrue("Sign in button is not displayed", isButtonSignInVisible());
+        System.out.println("Sign in button is displayed");
+        Assert.assertFalse("Sign out button is displayed", isButtonSignOutVisible());//
+        System.out.println("Sign out button is not displayed");
+        Assert.assertTrue("Error message is not displayed", isInvalidDataMessegeVisible());
+        System.out.println("Error message is displayed");
+
+    }
+
+
     private boolean isButtonSignInVisible() {
         try {
             return webDriver.findElement(By.xpath(".//button[text()='Sign In']")).isDisplayed();
