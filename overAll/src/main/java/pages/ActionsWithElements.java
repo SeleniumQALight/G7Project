74ab5a7551e0e04e2ecc47d729eab95b936aa734
tradulenko <radulenko@gmail.com>
2023-07-18 21:41:37 +0300

    LoginTestWithPageObject

--- overAll/src/main/java/pages/ActionsWithElements.java
+++ overAll/src/main/java/pages/ActionsWithElements.java
@@ -3,12 +3,15 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class ActionsWithElements {
     protected WebDriver webDriver;
 
     public ActionsWithElements(WebDriver webDriver) {
         this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this); // this - means all elements from this class will be initialized
+        // elements in @FindBy
     }
 
     public void openPage(String url) {
@@ -47,6 +50,10 @@ public class ActionsWithElements {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    public void checkElementDisplayed(WebElement element) {
+        Assert.assertTrue("Element is not displayed", isElementDisplayed(element));
     }
 
     private void printErrorAndStopTest(Exception e) {
