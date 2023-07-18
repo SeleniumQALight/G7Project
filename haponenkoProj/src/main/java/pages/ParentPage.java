329ed1ef45825fa6006d6d5c35b963cd0ecee0a5
iryna_haponenko <irinkagaponenko@gmail.com>
2023-07-18 21:41:37 +0300

    LoginTestWithPageObject

---
+++ haponenkoProj/src/main/java/pages/ParentPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ParentPage extends ActionsWithElements {
+    final String BASE_URL = "https://qa-complexapp.onrender.com";
+
+    public ParentPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+}
---
+++ haponenkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -0,0 +1,15 @@
+package loginTest;
+
+import org.junit.Test;
+
+public class LoginTestWithPageObject extends baseTest.BaseTest{
+    @Test
+    public void validLogin (){
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoInputUserName("qaauto");
+        pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+
+        //TODO Assert
+    }
+}
