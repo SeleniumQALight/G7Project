597652cddd247dc3d48fee2098961cc1c9c7c5f0
VladLubynets <hirgoff@gmail.com>
2023-07-20 00:46:30 +0300

    LoginTestWithPageObject

---
+++ lubynetsProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -0,0 +1,17 @@
+package loginTests;
+
+import org.junit.Test;
+
+public class LoginTestWithPageObject extends baseTest.BaseTest {
+    @Test
+    public void validLogin() {
+        pageProvider.getloginPage().openLoginPage();
+        pageProvider.getloginPage().enterTextIntoInputUserName("qaauto");
+        pageProvider.getloginPage().enterTextIntoInputPassword("123456qwerty");
+        pageProvider.getloginPage().clickOnButtonSignIn();
+
+        //TODO Assert
+
+    }
+
+}
