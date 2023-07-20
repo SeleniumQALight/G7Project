d33a550af9f803b42f0ab304189025bbcddff046
olehnalbatov <oleh.nalbatov@htmi.ch>
2023-07-20 21:37:16 +0300

    Header

---
+++ nalbatovProj/src/main/java/pages/elements/Header.java
@@ -0,0 +1,20 @@
+package pages.elements;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pages.ActionsWithElements;
+
+public class Header extends ActionsWithElements {
+
+    @FindBy(xpath = "//button[text()='Sign Out']")
+    private WebElement buttonSignOut;
+
+    public Header(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public void checkIsButtonSignOutVisible() {
+        checkElementDisplayed(buttonSignOut);
+    }
+}
