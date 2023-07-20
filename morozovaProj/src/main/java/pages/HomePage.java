0b2b273d190b66f968a46e7c9844f62784bfa3f0
MarynaMorozova <marisun.m.2015@gmail.com>
2023-07-20 21:33:34 +0300

    LoginTestWithPageObject+ HomePage + Header

---
+++ morozovaProj/src/main/java/pages/HomePage.java
@@ -0,0 +1,9 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends ParentPageWithHeder {
+    public HomePage(WebDriver webDriver) {
+        super(webDriver);
+    }
+}
--- morozovaProj/src/test/java/loginTests/LoginTestWithPageObject.java
+++ morozovaProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -2,14 +2,18 @@ package loginTests;
 
 import org.junit.Test;
 
+import static data.TestData.LOGIN_DEFAULT;
+import static data.TestData.PASSWORD_DEFAULT;
+
 public class LoginTestWithPageObject extends baseTest.BaseTest {
 
     @Test
     public void validLogin() {
         pageProvider.getloginPage().openLoginPage();
-        pageProvider.getloginPage().enterTextIntoInputUserNane("aqauto");
-        pageProvider.getloginPage().enterTextIntoInputPassword("123456qwerty");
+        pageProvider.getloginPage().enterTextIntoInputUserNane(LOGIN_DEFAULT);
+        pageProvider.getloginPage().enterTextIntoInputPassword(PASSWORD_DEFAULT);
         pageProvider.getloginPage().clickOnButtonSignIn();
+        pageProvider.getHomePage().getHeader().checkIsButtonSignOutVisible();
 
 // TODO Assert
     }
