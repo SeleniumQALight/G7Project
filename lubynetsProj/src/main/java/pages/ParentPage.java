845efdb66814d23b05ad399dee283891094ae721
VladLubynets <hirgoff@gmail.com>
2023-07-22 23:11:17 +0300

    refactroring

--- lubynetsProj/src/main/java/pages/ParentPage.java
+++ lubynetsProj/src/main/java/pages/ParentPage.java
@@ -1,11 +1,21 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 
 public class ParentPage extends ActionWithElements {
-    final String BASE_URL = "https://qa-complexapp.onrender.com/";
+    final String BASE_URL = "https://aqa-complexapp.onrender.com/";
 
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
+    }
+    public void openPage(String url) {
+        try {
+            webDriver.get(url);
+            logger.info("Page was opened " + url);
+        } catch (Exception e) {
+            logger.error("Can not open " + url);
+            Assert.fail("Can not open " + url);
+        }
     }
 }
