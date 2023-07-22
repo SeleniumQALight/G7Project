3d72b952122e8b2e5e320d4d9cf37e26171bcaac
TetianaKovalenko <tetyana.kovalenko.a@gmail.com>
2023-07-22 16:27:19 +0300

    inValidLogin with Page Object

--- kovalenkoProj/src/main/java/pages/elements/Header.java
+++ kovalenkoProj/src/main/java/pages/elements/Header.java
@@ -8,11 +8,29 @@ import pages.ActionsWithElements;
 public class Header extends ActionsWithElements {
     @FindBy(xpath = "//button[text() = 'Sign Out']")
     private WebElement buttonSignOut;
+
     public Header(WebDriver webDriver) {
         super(webDriver);
     }
 
     public void checkIsButtonSignOutVisible() {
         checkElementDisplayed(buttonSignOut);
+    }
+    public void checkIsButtonSignOutNotVisible() {
+        checkElementNotDisplayed(buttonSignOut);
+    }
+
+    @FindBy(xpath = "//button[text() = 'Sign In']")
+    private WebElement buttonSignIn;
+
+    public void checkIsButtonSignInVisible() {
+        checkElementDisplayed(buttonSignIn);
+    }
+
+    @FindBy(xpath = "//*[@class='alert alert-danger text-center']")
+    private WebElement alertMessageWrongLoginOrPassword;
+
+    public void checkIsAlertMessageVisible() {
+        checkElementDisplayed(alertMessageWrongLoginOrPassword);
     }
 }
--- kovalenkoProj/src/test/java/loginTests/LoginTestWithPageObject.java
+++ kovalenkoProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -10,7 +10,16 @@ public class LoginTestWithPageObject extends baseTest.BaseTest{
         pageProvider.getLoginPage().enterTextIntoInputUserName(TestData.LOGIN_DEFAULT);
         pageProvider.getLoginPage().enterTextIntoInputPassword(TestData.PASSWORD_DEFAULT);
         pageProvider.getLoginPage().clickOnButtonSignIn();
-
         pageProvider.getHomePage().getHeader().checkIsButtonSignOutVisible();
+    }
+    @Test
+    public void invalidLogin(){
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoInputUserName(TestData.INVALID_LOGIN);
+        pageProvider.getLoginPage().enterTextIntoInputPassword(TestData.INVALID_PASSWORD);
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+        pageProvider.getHomePage().getHeader().checkIsButtonSignOutNotVisible();
+        pageProvider.getHomePage().getHeader().checkIsButtonSignInVisible();
+        pageProvider.getHomePage().getHeader().checkIsAlertMessageVisible();
     }
 }
