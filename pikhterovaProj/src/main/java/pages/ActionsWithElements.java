48ef1a73a44e456705bf52774d1867ebe707eb29
ypikhterova <juliaokopna@gmail.com>
2023-07-27 21:51:49 +0300

    wait + Util

--- pikhterovaProj/src/main/java/pages/ActionsWithElements.java
+++ pikhterovaProj/src/main/java/pages/ActionsWithElements.java
@@ -5,16 +5,24 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ActionsWithElements {
 
     Logger logger = Logger.getLogger(getClass());
     protected WebDriver webDriver;
 
+    protected WebDriverWait webDriverWait10, webDriverWait15;
+
     public ActionsWithElements(WebDriver webDriver) {
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this);//this - means all elements from this class will be initialized FindBy
+        webDriverWait10 = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        webDriverWait15 = new WebDriverWait(webDriver, Duration.ofSeconds(15));
     }
 
     public void enterTextIntoInput(WebElement input, String text) {
@@ -34,6 +42,7 @@ public class ActionsWithElements {
 
     public void clickOnElement(WebElement element) {
         try {
+            webDriverWait10.until(ExpectedConditions.elementToBeClickable(element));
             element.click();
             logger.info("Element was clicked");
         } catch (Exception e) {
--- pikhterovaProj/src/test/java/postTests/CreatePost.java
+++ pikhterovaProj/src/test/java/postTests/CreatePost.java
@@ -1,18 +1,23 @@
 package postTests;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreatePost extends BaseTest {
+    private String title = "TC01 - New Post Pikhterova" + Util.getDateAndTimeFormatted();
     @Test
-    public void createNewPost(){
+    public void createNewPost() {
         pageProvider.getHomePage().openHomePage().checkIsRedirectToHomePage()
                 .getHeader().clickOnButtonCreatePost()
                 .checkIsRedirectToCreatePostPage()
-                .enterTextIntoInputTitle("New Post Pikhterova")
+                .enterTextIntoInputTitle(title)
                 .enterTextIntoInputBody("New Post Body")
                 .selectTextInDropDown("Приватне повідомлення")
                 //.selectValueInDropDown("One Person")
-                .clickOnButtonSaveNewPost();
+                .clickOnButtonSaveNewPost()
+                .checkTextInSuccessMessage("New post successfully created.");
+
+
     }
 }
