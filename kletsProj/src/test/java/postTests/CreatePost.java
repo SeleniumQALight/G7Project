c509833cada1f2b2821ad40fc5e15f684f7f95d6
Natali <klets.natali@gmail.com>
2023-07-27 11:51:49 -0700

    wait+Util class

--- kletsProj/src/test/java/postTests/CreatePost.java
+++ kletsProj/src/test/java/postTests/CreatePost.java
@@ -1,19 +1,22 @@
 package postTests;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreatePost extends BaseTest {
+    private String title = "TC01 - New Post Klets " + Util.getDateAndTimeFormatted();
     @Test
     public void createNewPost() {
         pageProvider.getHomePage().openHomePage().checkIsRedirectToHomePage()
                 .getHeader().clickOnButtonCreatePost()
                 .checkIsRedirectToCreatePostPage()
-                .enterTextIntoInputTitle("New Post Klets")
+                .enterTextIntoInputTitle(title)
                 .enterTextIntoInputBody("New Post Body Klets")
                // .selectTextInDropDown("Приватне повідомлення")
                  .selectValueInDropDown("One Person")
                 .clickOnButtonSaveNewPost()
+                .checkTextInSuccessMessage("New post successfully created.")
         ;
     }
 
