b8889413ea56926fe78cf301ff0a0d43644a56e0
TetianaKovalenko <tanya.kovalenko.a@gmail.com>
2023-07-27 21:51:46 +0300

    Wait + Util

--- kovalenkoProj/src/main/java/pages/ActionsWithElements.java
+++ kovalenkoProj/src/main/java/pages/ActionsWithElements.java
@@ -5,16 +5,24 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ActionsWithElements {
 
     Logger logger = Logger.getLogger(getClass());
     protected WebDriver webDriver;
+    protected WebDriverWait webDriverWait10, webDriverWait15;
 
     public ActionsWithElements(WebDriver webDriver) {
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this); // this - means all elements from this class will be initialized elements in FindBy
+        webDriverWait10 = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        webDriverWait15 = new WebDriverWait(webDriver, Duration.ofSeconds(15));
     }
 
     public void enterTextIntoInput(WebElement input, String text) {
@@ -29,6 +37,7 @@ public class ActionsWithElements {
 
     public void clickOnElement(WebElement element) {
         try {
+            webDriverWait10.until(ExpectedConditions.elementToBeClickable(element));
             element.click();
             logger.info("Element was clicked");
         } catch (Exception e) {
--- kovalenkoProj/src/test/java/postTests/CreatePost.java
+++ kovalenkoProj/src/test/java/postTests/CreatePost.java
@@ -1,18 +1,21 @@
 package postTests;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreatePost extends BaseTest {
+    private String title = "TC01 - New Post Tetiana" + Util.getDateAndTimeFormatted();
 
     @Test
     public void createNewPost() {
         pageProvider.getHomePage().openHomePage().checkIsRedirectToHomePage().getHeader()
                 .clickOnButtonCreatePost().checkIsRedirectToCreatePostPage()
-                .enterTextIntoInputTitle("New Post Tetiana").enterTextIntoInputBody("Body of the new Post Tetiana")
+                .enterTextIntoInputTitle(title).enterTextIntoInputBody("Body of the new Post Tetiana")
                 .selectTextInDropDown("Приватне повідомлення")
                 //.selectValueInDropDown("One Person")
-                .clickOnButtonSaveNewPost();
+                .clickOnButtonSaveNewPost()
+                .checkTextInSuccessMessage("New post successfully created.");
 
     }
 
