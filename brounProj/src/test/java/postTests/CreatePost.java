b5d3369013ad1f9771497a9b32098f968abd3831
Brown69 <brown.dmitry@gmail.com>
2023-07-27 21:51:56 +0300

    Waits+Utils

--- brounProj/src/test/java/postTests/CreatePost.java
+++ brounProj/src/test/java/postTests/CreatePost.java
@@ -1,20 +1,24 @@
 package postTests;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreatePost extends BaseTest {
+    private String title = "TC01 - New Post Dmytro" + Util.getDateAndTimeFormatted();
+
     @Test
     public void createNewPost(){
         pageProvider.getHomePage()
                 .openHomePage().checkIsRedirectToHomePage()
                 .getHeader().clickOnButtonCreatePost()
                 .checkIsRedirectToCreatePostPage()
-                .enterTextIntoInputTitle("New Post Dmytro")
+                .enterTextIntoInputTitle(title)
                 .enterTextIntoInputBody("New Post Body Dmytro")
                 .selectTextInDropDown("Приватне повідомлення")
                 //.selectValueInDropDown("One person")
                 .clickOnButtonSaveNewPost()
+                .checkTextInSuccessMessage("New post successfully created.")
         ;
 
     }
