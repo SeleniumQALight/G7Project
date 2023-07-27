995032814e5b7264e54496154d41638f00e0d762
KostiaMoroz7 <morozkostia7@gmail.com>
2023-07-27 21:54:41 +0300

    CreatePost Test Updated With Unique Name

--- morozProj/src/test/java/postTests/CreatePost.java
+++ morozProj/src/test/java/postTests/CreatePost.java
@@ -1,18 +1,21 @@
 package postTests;
 
 import baseTest.BaseTest;
+import libs.Util;
 import org.junit.Test;
 
 public class CreatePost extends BaseTest {
+
+    private String title = "TC01 - New Post" + Util.getDateAndTimeFormatted();
     @Test
     public void createNewPost(){
         pageProvider.getHomePage()
                 .openHomePage().checkIsRedirectToHomePage()
                 .getHeader().clickOnButtonCreatePost()
                 .checkIsRedirectToCreatePostPage()
-                .enterTextIntoInputTitle("New Post")
+                .enterTextIntoInputTitle(title)
                 .enterTextIntoInputBody("New Post Body")
                 .selectTextInDropDown("Приватне повідомлення")//.selectValueInDropDownByValue("One Person")
-                .clickOnButtonSave();
+                .clickOnButtonSave().checkTextInSuccessMessage("New post successfully created.");
     }
 }
