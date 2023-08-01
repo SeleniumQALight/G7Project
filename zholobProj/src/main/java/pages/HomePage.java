e425f68243c94480729f3d6934d79f28ab856d85
zinna2008 <innazaharova1971@gmail.com>
2023-08-01 21:57:09 +0300

    Zholob_01_08

--- zholobProj/src/main/java/pages/HomePage.java
+++ zholobProj/src/main/java/pages/HomePage.java
@@ -21,4 +21,15 @@ public class HomePage extends ParentPageWithHeder { //
         return this;
     }
 
+    public HomePage openHomePageAndLoginifNeeded() {
+        LoginPage loginPage = new LoginPage(webDriver); // створюємо об'єкт класу LoginPage
+        loginPage.openLoginPage();
+        if(this.getHeader().isButtonSignOutVisible()) { // перевірка чи є кнопка SignOut
+           logger.info("User is already logged in");
+        } else {
+            loginPage.loginWithValidCreds();
+            checkIsRedirectToHomePage();
+        }
+        return this;
+    }
 }
