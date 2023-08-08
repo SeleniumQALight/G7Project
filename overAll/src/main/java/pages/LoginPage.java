9e429565ab6218959796c84715432df2c18da31d
tradulenko <radulenko@gmail.com>
2023-08-08 20:48:03 +0300

    checkUrl methods were added

--- overAll/src/main/java/pages/LoginPage.java
+++ overAll/src/main/java/pages/LoginPage.java
@@ -33,14 +33,24 @@ public class LoginPage extends ParentPage{
     @FindBy(id = "password-register")
     private WebElement inputPasswordRegistration;
 
+    //same as webDriver.findElements(By.xpath(".//div[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']"));
+  //  @FindBy(xpath = ".//div[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']")
+  //  private List<WebElement> alertDanger;
+
     final String listErrorsMessagesLocator = ".//div[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']";
 
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
 
+    @Override
+    protected String getRelativeUrl() {
+        return "/";
+    }
+
     public void openLoginPage() {
         openPage(BASE_URL);
+        checkUrl();
     }
 
     public void enterTextIntoInputUserName(String userName) {
--- overAll/src/main/java/pages/ParentPage.java
+++ overAll/src/main/java/pages/ParentPage.java
@@ -3,8 +3,9 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 
-public class ParentPage extends ActionsWithElements{
+abstract public class ParentPage extends ActionsWithElements{
     final String BASE_URL = "https://aqa-complexapp.onrender.com";
+
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -18,4 +19,32 @@ public class ParentPage extends ActionsWithElements{
             Assert.fail("Can not open " + url);
         }
     }
+
+    abstract protected String getRelativeUrl();
+
+    //check url
+    // https://aqa-complexapp.onrender.com/ == BASE_URL + "/" -> true
+    protected void checkUrl(String relativeUrl) {
+            Assert.assertEquals("Url is not expected", BASE_URL + relativeUrl, webDriver.getCurrentUrl());
+    }
+
+    protected void checkUrl() {
+        checkUrl(getRelativeUrl());
+    }
+
+    //https://aqa-complexapp.onrender.com/post/64d21e84903640003414c338
+    // regex for 64d21e84903640003414c338
+    // [a-zA-Z0-9]{24}
+    //https://aqa-complexapp.onrender.com/post/[a-zA-Z0-9]
+    protected void checkUrlWithPattern(String relativeUrl) {
+        Assert.assertTrue("Url is not expected \n"
+                        + "Expected result: " + BASE_URL + relativeUrl + "\n"
+                        + "Actual result: " + webDriver.getCurrentUrl()
+                , webDriver.getCurrentUrl().matches(BASE_URL + relativeUrl ));
+    }
+
+    protected void checkUrlWithPattern() {
+        checkUrlWithPattern(getRelativeUrl());
+    }
+
 }
