b68cf51f86ae987b4d4ccd96dd913b35ac6927e9
olehnalbatov <oleh.nalbatov@htmi.ch>
2023-08-08 23:09:50 +0300

    run on env

--- nalbatovProj/src/main/java/pages/ParentPage.java
+++ nalbatovProj/src/main/java/pages/ParentPage.java
@@ -3,8 +3,9 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 
-public class ParentPage extends ActionsWithElements{
-    final String BASE_URL = "https://aqa-complexapp.onrender.com";
+abstract public class ParentPage extends ActionsWithElements {
+    String env = System.getProperty("env", "aqa");
+    final String BASE_URL = String.format("https://%s-complexapp.onrender.com", env);
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -17,4 +18,32 @@ public class ParentPage extends ActionsWithElements{
             Assert.fail("Can not open " + url);
         }
     }
+    abstract protected String getRelativeUrl();
+
+    //check url
+    //https://aqa-complexapp.onrender.com/ == BASE_URL + "/" -> true
+    protected void checkUrl(String relativeUrl) {
+        Assert.assertEquals("Url is not expected", BASE_URL + relativeUrl, webDriver.getCurrentUrl());
+    }
+
+    protected void checkUrl() {
+        checkUrl(getRelativeUrl());
+    }
+
+    //https://aqa-complexapp.onrender.com/post/5f9d4d3b1d5f1d0017f6b0e2
+    // regex for post id 5f9d4d3b1d5f1d0017f6b0e2
+    // [a-zA-Z0-9]{24}
+    //https://aqa-complexapp.onrender.com/post/[a-zA-Z0-9]{24}
+
+    protected void checkUrlWithPattern(String relativeUrl) {
+        Assert.assertTrue("Url is not expected \n" +
+                        "Expected result: " + BASE_URL + relativeUrl + "\n" +
+                        "Actual result: " + webDriver.getCurrentUrl()
+                , webDriver.getCurrentUrl().matches(BASE_URL + relativeUrl));
+    }
+
+    protected void checkUrlWithPattern() {
+        checkUrlWithPattern(getRelativeUrl());
+    }
 }
+
