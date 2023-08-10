7273cfd658e3bd156b4475bc805213906cc3ab77
mykhailo.kalinichenko <kalinichencomixail@gmail.com>
2023-08-10 20:34:20 +0300

    confighiddenProperties change ConfigProvicer was added

--- kalinichenkoProj/src/main/java/pages/ActionsWithElements.java
+++ kalinichenkoProj/src/main/java/pages/ActionsWithElements.java
@@ -1,5 +1,6 @@
 package pages;
 
+import libs.ConfigProvider;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
@@ -20,7 +21,7 @@ public class ActionsWithElements {
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this); // this - means all elements from this class will be initialized elements in FindBy
         webDriverWait10 = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-        webDriverWait15 = new WebDriverWait(webDriver, Duration.ofSeconds(15));
+        webDriverWait15 = new WebDriverWait(webDriver, Duration.ofSeconds(ConfigProvider.configProperties.TIME_FOR_EXPLICIT_WAIT_LOW()));
     }
 
     public void enterTextIntoInput(WebElement input, String text) {
