cd0944b44e9a8dd1e15a7d95ffb409941db77131
olehnalbatov <oleh.nalbatov@htmi.ch>
2023-08-10 21:40:57 +0300

    click on element by xpath

--- nalbatovProj/src/main/java/pages/ActionsWithElements.java
+++ nalbatovProj/src/main/java/pages/ActionsWithElements.java
@@ -3,6 +3,7 @@ package pages;
 import libs.ConfigProvider;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
@@ -41,6 +42,15 @@ public class ActionsWithElements {
         try {
             webDriverWait10.until(ExpectedConditions.elementToBeClickable(element));
             element.click();
+            logger.info("Element was clicked");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    public void clickOnElement(String locator) {
+        try {
+            clickOnElement(webDriver.findElement(By.xpath(locator)));
             logger.info("Element was clicked");
         } catch (Exception e) {
             printErrorAndStopTest(e);
