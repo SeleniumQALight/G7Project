9fe13e17382c145a6218739d7b046dc095c0fe6d
tradulenko <radulenko@gmail.com>
2023-08-10 20:44:18 +0300

    hiddenConfig was added

--- overAll/src/main/java/pages/ActionsWithElements.java
+++ overAll/src/main/java/pages/ActionsWithElements.java
@@ -1,5 +1,6 @@
 package pages;
 
+import libs.ConfigProvider;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
@@ -22,7 +23,7 @@ public class ActionsWithElements {
         PageFactory.initElements(webDriver, this); // this - means all elements from this class will be initialized
         // elements in @FindBy
         webDriverWait10 = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-        webDriverWait15 = new WebDriverWait(webDriver, Duration.ofSeconds(15));
+        webDriverWait15 = new WebDriverWait(webDriver, Duration.ofSeconds(ConfigProvider.configProperties.TIME_FOR_EXPLICIT_WAIT_LOW()));
     }
 
 
