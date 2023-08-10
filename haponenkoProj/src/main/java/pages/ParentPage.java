fc6849b2f977271ab1b1a436ad927b872e7eaaee
iryna_haponenko <irinkagaponenko@gmail.com>
2023-08-10 12:59:34 +0300

    checkUrl methods were added

--- haponenkoProj/src/main/java/pages/ParentPage.java
+++ haponenkoProj/src/main/java/pages/ParentPage.java
@@ -3,7 +3,7 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 
-public class ParentPage extends ActionsWithElements {
+ abstract public class ParentPage extends ActionsWithElements {
     final String BASE_URL = "https://aqa-complexapp.onrender.com";
 
     public ParentPage(WebDriver webDriver) {
@@ -19,4 +19,33 @@ public class ParentPage extends ActionsWithElements {
             Assert.fail("Can't open " + url);
         }
     }
+
+    abstract protected String getRelativeUrl();
+
+    //check URL
+    // https://aqa-complexapp.onrender.com/ == BASE_URL + "/" -> true
+    protected void checkUrl(String relativeUrl) {
+        Assert.assertEquals("Url is not expected", BASE_URL + relativeUrl, webDriver.getCurrentUrl());
+    }
+
+     protected void checkUrl() {
+         checkUrl(getRelativeUrl());
+     }
+
+    //https://aqa-complexapp.onrender.com/post/64d21e84903640003414c338
+    // regex for 64d21e84903640003414c338
+    // [a-zA-Z0-9]{24}
+    ////https://aqa-complexapp.onrender.com/post/[a-zA-Z0-9] регулярний вираз або паттерн для порівняння
+
+    protected void checkUrlWithPattern(String relativeUrl) {
+        Assert.assertTrue("Url is not expected \n"
+                        + "Expected result: " + BASE_URL + relativeUrl + "\n"
+                        + "Actual result: " + webDriver.getCurrentUrl()
+                , webDriver.getCurrentUrl().matches(BASE_URL + relativeUrl));
+    }
+
+     protected void checkUrlWithPattern() {
+         checkUrlWithPattern(getRelativeUrl());
+     }
+
 }
