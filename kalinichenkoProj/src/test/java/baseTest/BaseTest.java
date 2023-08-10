a90b7279d5cf60599a53c3300e0ce7fc8c043949
mykhailo.kalinichenko <kalinichencomixail@gmail.com>
2023-08-10 22:12:43 +0300

    cross browsers added click on element was added

--- kalinichenkoProj/src/test/java/baseTest/BaseTest.java
+++ kalinichenkoProj/src/test/java/baseTest/BaseTest.java
@@ -7,6 +7,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import pages.PageProvider;
 
 import java.time.Duration;
@@ -20,8 +24,7 @@ public class BaseTest {
 
     @Before
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+        webDriver = InitDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(ConfigProvider.configProperties.TIME_FOR_DEFAULT_WAIT()));
         logger.info("Browser was opened");
@@ -32,6 +35,29 @@ public class BaseTest {
     public void tearDown() {
         webDriver.quit();
         logger.info("Browser was closed");
+    }
+
+    private WebDriver InitDriver() {
+        String browser = System.getProperty("browser");
+        if ((browser == null) || "chrome".equals(browser.toLowerCase())) {
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+        } else if ("firefox".equals(browser.toLowerCase())) {
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        } else if ("ie".equals(browser.toLowerCase())) {
+            WebDriverManager.iedriver().setup(); // zoom 100%
+            webDriver = new InternetExplorerDriver(); // security level - medium
+        } else if ("safari".equals(browser.toLowerCase())) {
+            WebDriverManager.safaridriver().setup();
+            webDriver = new SafariDriver();
+        } else if ("edge".equals(browser.toLowerCase())) {
+            WebDriverManager.edgedriver().setup();
+            webDriver = new EdgeDriver();
+        }
+        else {throw new IllegalArgumentException("Browser " + browser + " is not supported");
+        }
+        return webDriver;
     }
 
 }
