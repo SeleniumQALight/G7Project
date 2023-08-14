fe59ed8a132717879e8d632cda7805c95ebf6a60
zinna2008 <innazaharova1971@gmail.com>
2023-08-14 19:03:16 +0300

    EDIT_POST_TEST

--- zholobProj/src/main/java/pages/MyProfilePage.java
+++ zholobProj/src/main/java/pages/MyProfilePage.java
@@ -7,8 +7,9 @@ import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
-public class MyProfilePage extends ParentPageWithHeder{
+public class MyProfilePage extends ParentPageWithHeder {
     private String postTitleLocator = "//*[text()='%s']"; // змінна для локатора заголовка поста
+
     public MyProfilePage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -18,15 +19,15 @@ public class MyProfilePage extends ParentPageWithHeder{
         return "/profile/[a-zA-Z0-9]*"; // регулярний вираз для перевірки чи ми на сторінці профілю
     }
 
-    public MyProfilePage checkIsRedirectToMyProfilePage() {
-        checkUrlWithPattern(); //TODO check url
+    public MyProfilePage checkIsRedirectToMyProfilePage() { // перевірка чи ми на сторінці профілю
+        checkUrlWithPattern(); // check url
         return this;
 
 
         //TODO check unique element
     }
 
-    private List<WebElement> getPostList(String title) {
+    private List<WebElement> getPostList(String title) { // метод для отримання списку постів з вказаним заголовком
         return webDriver.findElements(By.xpath(
                 String.format(postTitleLocator, title)
         ));
@@ -34,7 +35,7 @@ public class MyProfilePage extends ParentPageWithHeder{
 
     }
 
-    public MyProfilePage checkpostWithTitleIsPresent(String title) {
+    public MyProfilePage checkpostWithTitleIsPresent(String title) { // метод для перевірки чи є пост з вказаним заголовком і він один
         Assert.assertEquals("Count of posts with title " + title,
                 1,
                 getPostList(title).size());
@@ -43,26 +44,34 @@ public class MyProfilePage extends ParentPageWithHeder{
 
     }
 
-    public MyProfilePage deletePostsTillPresent(String title) {
-List<WebElement> postsList = getPostList(title);
 
-int counter = 0;
-while (!postsList.isEmpty() && counter < 100) {
+    public MyProfilePage deletePostsTillPresent(String title) { // метод для видалення постів з вказаним заголовком
+        List<WebElement> postsList = getPostList(title);
 
-    clickOnElement(getPostList(title).get(0));
-    new PostPage(webDriver)
-            .checkIsRedirectToPostPage()
-            .clickOnDeletePostButton()
-            .checkIsRedirectToMyProfilePage();
-    logger.info("Post with title " + title + " was deleted");
-    postsList = getPostList(title);
-    counter++;
+        int counter = 0;
+        while (!postsList.isEmpty() && counter < 100) {
 
-}
-if (counter >= 100) {
-    Assert.fail("There are more than 100 posts with title " + title );
-}
+            clickOnElement(getPostList(title).get(0));
+            new PostPage(webDriver)
+                    .checkIsRedirectToPostPage()
+                    .clickOnDeletePostButton()
+                    .checkIsRedirectToMyProfilePage();
+            logger.info("Post with title " + title + " was deleted");
+            postsList = getPostList(title);
+            counter++;
+
+        }
+        if (counter >= 100) {
+            Assert.fail("There are more than 100 posts with title " + title);
+        }
 //
         return this;
     }
+
+    public PostPage clickOnPostWithTitle(String title) { // метод для кліку на пост з вказаним заголовком
+        clickOnElement(getPostList(title).get(0));
+        return new PostPage(webDriver);
+    }
+
+
 }
---
+++ zholobProj/src/test/java/EditPostTest/EditPostTest.java
@@ -0,0 +1,59 @@
+package EditPostTest;
+
+import baseTest.BaseTest;
+import libs.Util;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EditPostTest extends BaseTest {
+    private String title = "TC01 - New Post Inna" + Util.getDateAndTimeFormatted();
+    private String body = "Body of Post Inna";
+    private String title_edit = "TC01 - New Post Inna" + Util.getDateAndTimeFormatted() + "edit_22222222222";
+
+    @Before
+
+    public void createNewPost() {
+
+        pageProvider.getHomePage()
+                .openHomePage().checkIsRedirectToHomePage() //певірка чи ми на дошній сторінці
+                .getHeader().clickOnButtonCreatePost() // клік на кнопку CreatePost
+                .checkIsRedirectToCreatePostPage() //певірка чи ми на сторінці CreatePost
+                .enterTextIntoInputTitle(title)// вводимо текст в поле Title
+                .enterTextIntoInputBody(body)
+                .selectTextInDropDown2("Приватне повідомлення") // вибираємо значення з дропдауну по тексту
+                .selectOnCheckBoxIs("check") // вибираємо чекбокс
+                //.selectValueInDropDown("One Person")//
+                .clickOnButtonSaveNewPost()
+                .checkTextInSuccessMessage("New post successfully created."); // перевірка чи  повідомлення выдповідає тексту в елементі
+
+    }
+
+    @Test // редагування поста
+    public void editPost() {
+        pageProvider.getPostPage()
+                .getHeader().clickOnMyProfileButton()// перехід на сторінку профайлу
+                .checkIsRedirectToMyProfilePage()// перевірка чи ми на сторінці профайлу
+                .checkpostWithTitleIsPresent(title) // перевырка  чи є пост з таким заголовком і він один
+                .clickOnPostWithTitle(title); // клік на пост з заголовком
+        pageProvider.getPostPage().checkIsRedirectToPostPage() // перевірка чи ми на сторінці поста
+                .clickOnEditPostButton(); // клік на кнопку редагування поста
+        pageProvider.getPostEditPage().enterTextIntoInputTitle(title_edit)// вводимо новий текст в поле Title
+                .clickOnButtonSaveUpdates() // клік на кнопку збереження поста
+                .checkIsMessageSuccessfullyEditPost() // перевірка чи  є повідомлення про успішне редагування
+
+        ;
+    }
+
+    @After
+    public void deletePosts() {
+        pageProvider.getHomePage().openHomePageAndLoginifNeeded()
+                .openHomePageAndLoginifNeeded()
+                .getHeader().clickOnMyProfileButton()
+                //           .checkIsRedirectToMyProfilePage() // перевірка чи ми на сторінці профайлу
+                .deletePostsTillPresent(title) // видалення постів з таким заголовком
+                .deletePostsTillPresent(title_edit) // видалення постів з таким заголовком
+        ;
+    }
+
+}
