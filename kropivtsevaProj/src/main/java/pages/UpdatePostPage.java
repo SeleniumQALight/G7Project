7d8669efa2124da3210d3384441b251fad8ee679
Kropivtseva_Daria <kropivtseva.daria@gmail.com>
2023-08-15 16:53:17 +0300

    edit post

---
+++ kropivtsevaProj/src/main/java/pages/UpdatePostPage.java
@@ -0,0 +1,107 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+
+public class UpdatePostPage extends ParentPageWithHeader {
+
+
+    @FindBy(id = "post-title")
+    private WebElement inputTitle;
+
+    @FindBy(id = "post-body")
+    private WebElement inputBody;
+
+    @FindBy(xpath = ".//input[@type='checkbox']")
+    private WebElement checkbox;
+
+    @FindBy(tagName = "select")
+    private WebElement dropDownSelectValue;
+
+    @FindBy(xpath = ".//button[text()='Save Updates']")
+    private WebElement buttonSaveUpdates;
+
+    @FindBy(xpath = ".//div[@class='alert alert-success text-center']")
+    private WebElement successMessageElement;
+
+    @FindBy(xpath = "//input[@class='form-control form-control-lg form-control-title']")
+    private WebElement updatePostTitle;
+
+    private String postTitleLocator = ".//*[text()='%s']";
+
+
+    public UpdatePostPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    @Override
+    protected String getRelativeUrl() {
+        return "/post/[a-zA-Z0-9]*/edit";
+    }
+
+
+    public UpdatePostPage checkIsRedirectToUpdatePostPage() {
+        checkUrlWithPattern();
+        //TODO UNIQUE ELEMENT
+        return this;
+    }
+
+
+    public UpdatePostPage enterTextIntoInputTitle(String s) {
+        enterTextIntoInput(inputTitle, s);
+        return this;
+    }
+
+    public UpdatePostPage enterTextIntoInputBody(String bodyOfUpdatedPost) {
+        enterTextIntoInput(inputBody, bodyOfUpdatedPost);
+        return this;
+    }
+
+
+    public UpdatePostPage workWithCheckBox() {
+        setCheckStatusToCheckBox(checkbox);
+        return this;
+    }
+
+    public UpdatePostPage checkOrUncheckCheckBoxDependingOnText(String text) {
+        checkOrUncheckCheckBoxDependingOnText(checkbox, text);
+        return this;
+    }
+
+    public UpdatePostPage selectTextInDropDownByUI() {
+        selectTextInDropDownByUI(dropDownSelectValue, "Частное сообщение");
+        return this;
+    }
+
+    public UpdatePostPage clickOnButtonSaveUpdates() {
+        clickOnElement(buttonSaveUpdates);
+        return this;
+    }
+
+    public UpdatePostPage checkIsSuccessMessageDisplayed(String text) {
+        Assert.assertEquals("Text in message", text, successMessageElement.getText());
+        return this;
+    }
+
+    private List<WebElement> getPostsList(String title) {
+        return webDriver.findElements(By.xpath(String.format(postTitleLocator, title)));
+    }
+
+
+    public UpdatePostPage checkIsPostTitleUpdated(String title) {
+        Assert.assertEquals("Count of posts with title " + title, 0, getPostsList(title).size());
+        return this;
+    }
+
+    public UpdatePostPage checkIsTextIntoInputBody(String bodyOfUpdatedPost) {
+        Assert.assertEquals("Text in input body", bodyOfUpdatedPost, inputBody.getAttribute("value"));
+        return this;
+    }
+
+}
---
+++ kropivtsevaProj/src/test/java/postTests/UpdatePost.java
@@ -0,0 +1,67 @@
+package postTests;
+
+import baseTest.BaseTest;
+import libs.Util;
+import org.junit.After;
+import org.junit.Test;
+
+
+public class UpdatePost extends BaseTest {
+    private String title = "TC01 - New Post Kropivtseva " + Util.getDateAndTimeFormatted();
+    private String update = "update" + Util.getDateAndTimeFormatted();
+
+
+    @Test
+
+    public void updatePost() {
+        pageProvider.getHomePage()
+                .openHomePage()
+                .checkIsRedirectToHomePage()
+                .getHeader()
+                .clickOnButtonCreatePost()
+                .checkIsRedirectToCreatePostPage()
+                .enterTextIntoInputTitle(title)
+                .enterTextIntoInputBody("Body of new post")
+                .workWithCheckBox()
+                .checkOrUncheckCheckBoxDependingOnText("check")
+                .selectTextInDropDownByUI()
+                .clickOnButtonSaveNewPost()
+                .checkIsSuccessMessageDisplayed()
+                .checkTextInSuccessMessage("New post successfully created.")
+                .checkIsPostBodyDisplayed("Body of new post")
+                .checkIsPostTitleDisplayed(title)
+                .checkIsPostUnique("Is this post unique? : yes")
+                .checkIsPostPrivate("Note: This post was written for One Person")
+                .getHeader().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .clickOnPostWithTitle(title)
+                .checkIsRedirectToPostPage()
+                .clickOnButtonEditPost()
+                .checkIsRedirectToUpdatePostPage()
+                .enterTextIntoInputTitle(title + update)
+                .enterTextIntoInputBody("Body of updated post")
+//                .workWithCheckBox() - прибрала цей степ, тому-що там бага
+//                .checkOrUncheckCheckBoxDependingOnText("check") - прибрала цей степ, тому-що там бага
+//                .selectTextInDropDownByUI() - прибрала цей степ, тому-що там бага
+                .clickOnButtonSaveUpdates()
+                .checkIsSuccessMessageDisplayed("Post successfully updated.")
+                .checkIsPostTitleUpdated(title + update)
+                .checkIsTextIntoInputBody("Body of updated post")
+                .getHeader().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .checkPostWithTitleIsPresent(title + update);
+    }
+
+
+    @After
+    public void deletePost() {
+        pageProvider.getHomePage()
+                .openHomePageAndLoginIfNeeded()
+                .getHeader()
+                .clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .deletePostsTillPresent(title + update)
+        ;
+    }
+}
+
