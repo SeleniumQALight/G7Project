f5dc8820f7af3095db8511a74b1ec983c1847ef8
Taras Seliuk <taras.selyuk@gmail.com>
2023-08-15 17:22:44 +0300

    seliuk-08-08 - check URL methods was added

--- seliukProj/src/main/java/pages/ParentPage.java
+++ seliukProj/src/main/java/pages/ParentPage.java
@@ -3,7 +3,7 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 
-public class ParentPage extends ActionsWithElements {
+abstract public class ParentPage extends ActionsWithElements {
     final String BASE_URL = "https://aqa-complexapp.onrender.com"; //https://qa-complexapp.onrender.com
 
     public ParentPage(WebDriver webDriver) {
@@ -19,5 +19,37 @@ public class ParentPage extends ActionsWithElements {
             Assert.fail("Can not open URL " + url);
         }
     }
+
+    abstract protected String getRelativeUrl();
+
+    //check URL
+    //https://aqa-complexapp.onrender.com/ == BASE_URL + "/" -> true
+    protected void checkUrl(String relativeUrl) {
+        Assert.assertEquals("Url is not expected", BASE_URL + relativeUrl, webDriver.getCurrentUrl());
+    }
+
+    protected void checkUrl() {
+        checkUrl(getRelativeUrl());
+    }
+
+
+
+
+    //https://aqa-complexapp.onrender.com/post/64dac18b2eafa800337c3b25
+    //regex on 64dac18b2eafa800337c3b25
+    //[a-z0-9]{24}
+    //https://aqa-complexapp.onrender.com/post/[a-z0-9]
+    protected void checkUrlWithPattern(String relativeUrl) {
+        Assert.assertTrue("Url is not expected \n"
+                + "Expected result: " + BASE_URL + relativeUrl + "\n"
+                + "Actual result: " + webDriver.getCurrentUrl()
+                , webDriver.getCurrentUrl().matches(BASE_URL + relativeUrl));
+    }
+
+    protected void checkUrlWithPattern() {
+        checkUrlWithPattern(getRelativeUrl());
+    }
+
+
 
 }
--- seliukProj/src/main/java/pages/ParentPageWithHeader.java
+++ seliukProj/src/main/java/pages/ParentPageWithHeader.java
@@ -3,8 +3,8 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import pages.elements.Header;
 
-public class ParentPageWithHeader extends ParentPage {
-    Header header;
+abstract public class ParentPageWithHeader extends ParentPage {
+    Header header; 
 
     public ParentPageWithHeader(WebDriver webDriver) {
         super(webDriver);
