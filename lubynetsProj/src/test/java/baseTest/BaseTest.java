1218737382c429cc5df94ee3bf7c716c32fa91e8
VladLubynets <hirgoff@gmail.com>
2023-08-17 01:01:34 +0300

    Run on browsers

--- lubynetsProj/src/test/java/baseTest/BaseTest.java
+++ lubynetsProj/src/test/java/baseTest/BaseTest.java
@@ -7,6 +7,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.PageProvider;
 
@@ -22,8 +26,7 @@ public class BaseTest {
 
     @Before
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+       WebDriver webDriver = initDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(ofSeconds(ConfigProvider.configProperties.TIME_FOR_DEFAULT_WAIT()));
         WebDriverWait wait = new WebDriverWait(webDriver, ofSeconds(5));
@@ -36,5 +39,33 @@ public class BaseTest {
         webDriver.quit();
         logger.info("Browser was closed");
     }
+
+    private WebDriver initDriver() {
+        String browser = System.getProperty("browser");
+        if ((browser == null) || ("chrome".equals(browser.toLowerCase()))) {
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+        } else if ("firefox".equals(browser.toLowerCase())) {
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        } else if("iedriver".equals(browser.toLowerCase())) {
+            WebDriverManager.iedriver().setup(); // zoom 100%
+            webDriver = new InternetExplorerDriver();//level of security - Medium
+        }
+        else if("edgedriver".equals(browser.toLowerCase())) {
+            WebDriverManager.edgedriver().setup();
+            webDriver = new EdgeDriver();
+        }
+        else if ("safari".equals(browser.toLowerCase())) {
+            WebDriverManager.getInstance(SafariDriver.class).setup();
+            webDriver = new SafariDriver();
+        }
+        else {
+            throw new IllegalArgumentException("Can't init driver for browser " + browser);
+        }
+
+        return  webDriver;
+    }
+
 
 }
