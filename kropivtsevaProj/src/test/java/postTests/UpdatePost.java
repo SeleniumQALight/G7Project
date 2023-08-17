2533fb8e48e64c60ab13690e1f21288d0b86fa1d
Kropivtseva_Daria <kropivtseva.daria@gmail.com>
2023-08-17 19:04:44 +0300

    fix create post in Before

--- kropivtsevaProj/src/test/java/postTests/UpdatePost.java
+++ kropivtsevaProj/src/test/java/postTests/UpdatePost.java
@@ -3,6 +3,7 @@ package postTests;
 import baseTest.BaseTest;
 import libs.Util;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -10,10 +11,8 @@ public class UpdatePost extends BaseTest {
     private String title = "TC01 - New Post Kropivtseva " + Util.getDateAndTimeFormatted();
     private String update = "update" + Util.getDateAndTimeFormatted();
 
-
-    @Test
-
-    public void updatePost() {
+    @Before
+    public void createPost() {
         pageProvider.getHomePage()
                 .openHomePage()
                 .checkIsRedirectToHomePage()
@@ -31,7 +30,14 @@ public class UpdatePost extends BaseTest {
                 .checkIsPostBodyDisplayed("Body of new post")
                 .checkIsPostTitleDisplayed(title)
                 .checkIsPostUnique("Is this post unique? : yes")
-                .checkIsPostPrivate("Note: This post was written for One Person")
+                .checkIsPostPrivate("Note: This post was written for One Person");
+
+    }
+
+    @Test
+
+    public void updatePost() {
+        pageProvider.getHomePage()
                 .getHeader().clickOnMyProfileButton()
                 .checkIsRedirectToMyProfilePage()
                 .clickOnPostWithTitle(title)
