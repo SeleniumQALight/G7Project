e31eb4eadb977d7d5158ad4f43ddd9fdd458b84c
zinna2008 <innazaharova1971@gmail.com>
2023-08-24 21:47:43 +0300

    hiddenConfig.properties_edit

--- zholobProj/src/main/java/libs/ConfigHiddenProperties.java
+++ zholobProj/src/main/java/libs/ConfigHiddenProperties.java
@@ -2,7 +2,7 @@ package libs;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources(value = "file:C:\\111TESTIROVCHIK\\TTR\\G7_TAF\\G7Project\\zholobProj\\src\\main\\resources\\hiddenConfig.properties")
+@Config.Sources(value = "file:.src\\main\\resources\\hiddenConfig.properties")
 public interface ConfigHiddenProperties extends Config {
     String login();
     String password();
