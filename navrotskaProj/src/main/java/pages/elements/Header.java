88c87e873db3abeafc22e472d5cc2f212e268afb
olesyanavrotska <olesya.elopage@gmail.com>
2023-08-24 13:43:37 +0300

    added SmokeSuite and allure

--- navrotskaProj/src/main/java/pages/elements/Header.java
+++ navrotskaProj/src/main/java/pages/elements/Header.java
@@ -1,5 +1,6 @@
 package pages.elements;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -28,51 +29,51 @@ public class Header extends ActionsWithElements {
     public Header(WebDriver webDriver) {
         super(webDriver);
     }
-
+    @Step
     public void checkIsButtonSignOutVisible() {
         checkElementDisplayed(buttonSignOut);
     }
-
+    @Step
     public void clickOnButtonSignOut() {
         clickOnElement(buttonSignOut);
     }
-
+    @Step
     public void checkIsButtonSignOutNotVisible() {
         checkElementNotDisplayed(buttonSignOut);
     }
-
+    @Step
     public void checkIsButtonChatVisible() {
         checkElementDisplayed(buttonChat);
     }
-
+    @Step
     public void checkIsButtonChatNotVisible() {
         checkElementNotDisplayed(buttonChat);
     }
-
+    @Step
     public void checkIsButtonSearchVisible() {
         checkElementDisplayed(buttonSearch);
     }
-
+    @Step
     public void checkIsButtonSearchNotVisible() {
         checkElementNotDisplayed(buttonSearch);
     }
-
+    @Step
     public void checkIsButtonMyProfileVisible() {
         checkElementDisplayed(buttonMyProfile);
     }
-
+    @Step
     public void checkIsButtonMyProfileNotVisible() {
         checkElementNotDisplayed(buttonMyProfile);
     }
-
+    @Step
     public void checkIsButtonCreatePostVisible() {
         checkElementDisplayed(buttonCreatePost);
     }
-
+    @Step
     public void checkIsButtonCreatePostNotVisible() {
         checkElementNotDisplayed(buttonCreatePost);
     }
-
+    @Step
     public CreatePostPage clickOnButtonCreatePost() {
         clickOnElement(buttonCreatePost);
         return new CreatePostPage(webDriver);
