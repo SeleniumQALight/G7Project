e10c9ddce04aa7bc0a5196d501e5b26da2857630
iryna_haponenko <irinkagaponenko@gmail.com>
2023-08-29 17:20:31 +0300

    run UI tests

--- haponenkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ haponenkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -1,11 +1,15 @@
 package loginTest;
 
 import baseTest.BaseTest;
+import categories.SmokeTestFilter;
+import io.qameta.allure.*;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import libs.ConfigProvider;
 import libs.ExcelDriver;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
@@ -15,10 +19,22 @@ import static data.TestData.LOGIN_DEFAULT;
 import static data.TestData.PASSWORD_DEFAULT;
 
 @RunWith(JUnitParamsRunner.class)
+
+@Epic("Allure examples")
+@Feature("Junit 4 support")
 public class LoginTestWithPageObject extends BaseTest {
 
     final static String ERROR_MESSAGE = "Invalid username / pasword";
+    @Description("Some detailed test description")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Base support for bdd annotations")
     @Test
+    //@Ignore
+    @Category(SmokeTestFilter.class)
     public void validLogin() {
         pageProvider.getLoginPage().openLoginPage();
         pageProvider.getLoginPage().enterTextIntoInputUserName(LOGIN_DEFAULT);
