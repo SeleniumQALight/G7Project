3ae64d149ccfe001f4b59037b2c4b0845e9134ec
Brown69 <brown.dmitry@gmail.com>
2023-08-29 21:40:12 +0300

    API Test

---
+++ brounProj/src/test/java/apiTests/APITest.java
@@ -0,0 +1,23 @@
+package apiTests;
+
+import api.EndPoints;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class APITest {
+    final String USER_NAME = "autoapi";
+    @Test
+    public void getPostsByUserTest() {
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(EndPoints.POSTS_BY_USER, USER_NAME) // URL
+                .then()
+                .statusCode(200)
+                .log().all()
+        ;
+    }
+}
