8b54ac7efd7d724fa4b3c4c56ff46610e6792fdb
olesyanavrotska <olesya.elopage@gmail.com>
2023-08-29 21:40:28 +0300

    api test

---
+++ navrotskaProj/src/test/java/apiTests/EndPoints.java
@@ -0,0 +1,7 @@
+package apiTests;
+
+public interface EndPoints {
+    final String BASE_URL = "http://aqa-complexapp.onrender.com";
+    final String GET_POSTS_BY_USER = BASE_URL + "/api/postsByAuthor/{0}";
+    final String GET_POSTS_BY_ID = "/posts/";
+}
