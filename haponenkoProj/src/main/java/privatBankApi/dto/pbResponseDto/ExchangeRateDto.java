a16f0baf0cecfa2dad02253853c184a0981c0206
iryna_haponenko <irinkagaponenko@gmail.com>
2023-09-01 18:26:29 +0300

    hw Ex.1

---
+++ haponenkoProj/src/main/java/privatBankApi/dto/pbResponseDto/ExchangeRateDto.java
@@ -0,0 +1,80 @@
+package privatBankApi.dto.pbResponseDto;
+
+public class ExchangeRateDto {
+    String baseCurrency;
+    String currency;
+    double saleRateNB;
+    double purchaseRateNB;
+    double saleRate;
+    double purchaseRate;
+
+    public ExchangeRateDto() {
+    }
+
+    public ExchangeRateDto(String baseCurrency, String currency) {
+        this.baseCurrency = baseCurrency;
+        this.currency = currency;
+    }
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(String baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public double getSaleRateNB() {
+        return saleRateNB;
+    }
+
+    public void setSaleRateNB(double saleRateNB) {
+        this.saleRateNB = saleRateNB;
+    }
+
+    public double getPurchaseRateNB() {
+        return purchaseRateNB;
+    }
+
+    public void setPurchaseRateNB(double purchaseRateNB) {
+        this.purchaseRateNB = purchaseRateNB;
+    }
+
+    public double getSaleRate() {
+        return saleRate;
+    }
+
+    public void setSaleRate(double saleRate) {
+        this.saleRate = saleRate;
+    }
+
+    public double getPurchaseRate() {
+        return purchaseRate;
+    }
+
+    public void setPurchaseRate(double purchaseRate) {
+        this.purchaseRate = purchaseRate;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "ExchangeRateDto{" +
+                "baseCurrency='" + baseCurrency + '\'' +
+                ", currency='" + currency + '\'' +
+                ", saleRateNB=" + saleRateNB +
+                ", purchaseRateNB=" + purchaseRateNB +
+                ", saleRate=" + saleRate +
+                ", purchaseRate=" + purchaseRate +
+                '}';
+    }
+}
