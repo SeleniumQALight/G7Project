6eb875648ce0b8234b69de52e0a5163c57a8cc6e
iryna_haponenko <irinkagaponenko@gmail.com>
2023-09-05 19:35:51 +0300

    hw Ex.2 with fixes

--- haponenkoProj/src/test/java/privatBankApiTests/PrivatBankApiTest.java
+++ haponenkoProj/src/test/java/privatBankApiTests/PrivatBankApiTest.java
@@ -179,6 +179,7 @@ public class PrivatBankApiTest {
                 logger.info("Purchase Rate NB for Currency: " + exchangeRate.getCurrency() + " is " + exchangeRate.getPurchaseRateNB());
             }
         }
+
         softAssertions.assertAll();
     }
 }
