9fc956dabe6df4e4c3df3f5b6dd947cbbe472aba
mykhailo.kalinichenko <kalinichencomixail@gmail.com>
2023-09-05 18:01:19 +0300

    was added PrivatApiTest with additional checks

---
+++ kalinichenkoProj/src/main/java/privatbankApi/EndPoints.java
@@ -0,0 +1,7 @@
+package privatbankApi;
+
+public interface EndPoints {
+    final String BASE_URL = "https://api.privatbank.ua/p24api/exchange_rates";
+    final String DATE_STING = "date";
+    final String DATE = "22.03.2022";
+}
