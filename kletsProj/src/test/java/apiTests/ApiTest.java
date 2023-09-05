c4814ad33c516f8c2eb75758f5bc190e44da1a45
Natali <klets.natali@gmail.com>
2023-09-05 11:31:02 -0700

    ResponseAsString ResponseAsPath

--- kletsProj/src/test/java/apiTests/ApiTest.java
+++ kletsProj/src/test/java/apiTests/ApiTest.java
@@ -4,10 +4,14 @@ import api.EndPoints;
 import api.dto.responseDto.AuthorDto;
 import api.dto.responseDto.PostDto;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -58,12 +62,12 @@ public class ApiTest {
 
         SoftAssertions softAssertions = new SoftAssertions(); // soft assertion
 
-       // for (int i = 0; i< expectedPostDto.length; i++) { // loop through array of expected posts
+        // for (int i = 0; i< expectedPostDto.length; i++) { // loop through array of expected posts
 //            softAssertions.assertThat(responseAsDto[i])
 //                    .isEqualToIgnoringGivenFields(expectedPostDto[i], "id", "createdDate", "author"); // compare posts by fields
 //            softAssertions.assertThat(responseAsDto[i]
 //                    .getAuthor()).isEqualToIgnoringGivenFields(expectedPostDto[i].getAuthor(), "avatar"); // compare authors by fields
-                 //  }
+        //  }
 
         softAssertions.assertThat(responseAsDto)
                 .usingRecursiveComparison()
@@ -72,6 +76,54 @@ public class ApiTest {
 
         softAssertions.assertAll(); // soft assertion
 
+    }
+
+    @Test
+    public void getAllPostByUserNegative() {
+        String actualResponse =
+                given()
+                        .contentType(ContentType.JSON)
+                        .log().all()
+                        .when()
+                        .get(EndPoints.POSTS_BY_USER, "NotValidUser")
+                        .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract().response().body().asString();
+
+        Assert.assertEquals("Message in response is not matched ", "\"Sorry, invalid user requested.undefined\""
+                , actualResponse);
+        Assert.assertEquals("Message in response is not matched ", "Sorry, invalid user requested.undefined"
+                , actualResponse.replace("\"", ""));
+    }
+
+    @Test
+    public void getAllPostsByUserPath() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(EndPoints.POSTS_BY_USER, USER_NAME)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract().response();
+
+        List<String> actualTitlesList = response.jsonPath().getList("title", String.class);
+        SoftAssertions softAssertions = new SoftAssertions();
+        for (int i = 0; i < actualTitlesList.size(); i++) {
+            softAssertions.assertThat(actualTitlesList.get(i)).as("Item number " + i)
+                    .contains("test");
+        }
+
+        List<Map> actualAutorList = response.jsonPath().getList("author", Map.class);
+
+        for (int i = 0; i < actualAutorList.size(); i++) {
+            softAssertions.assertThat(actualAutorList.get(i).get("username")).as("Item number " + i)
+                    .isEqualTo(USER_NAME);
+        }
+
+        softAssertions.assertAll();
     }
 
 }
