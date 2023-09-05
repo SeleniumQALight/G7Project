d0560a411d991d75d9a180ef9489bfc8c2beff78
Taras Seliuk <taras.selyuk@gmail.com>
2023-09-05 20:30:52 +0300

    seliuk-05-09-lecture code refactoring

--- seliukProj/src/main/java/api/dto/responseDto/PostDto.java
+++ seliukProj/src/main/java/api/dto/responseDto/PostDto.java
@@ -1,7 +1,14 @@
 package api.dto.responseDto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
 public class PostDto {
     @JsonProperty("_id")
     String id;
@@ -15,93 +22,93 @@ public class PostDto {
 
     Boolean isVisitorOwner;
 
-    public PostDto() {
-    }
+//    public PostDto() {
+//    }
 
-    public PostDto(String title, String body, String select, String uniquePost, AuthorDto author, Boolean isVisitorOwner) {
-        this.title = title;
-        this.body = body;
-        this.select = select;
-        this.uniquePost = uniquePost;
-        this.author = author;
-        this.isVisitorOwner = isVisitorOwner;
-    }
+//    public PostDto(String title, String body, String select, String uniquePost, AuthorDto author, Boolean isVisitorOwner) {
+//        this.title = title;
+//        this.body = body;
+//        this.select = select;
+//        this.uniquePost = uniquePost;
+//        this.author = author;
+//        this.isVisitorOwner = isVisitorOwner;
+//    }
 
-    public String getId() {
-        return id;
-    }
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public String getBody() {
+//        return body;
+//    }
+//
+//    public String getSelect() {
+//        return select;
+//    }
+//
+//    public String getUniquePost() {
+//        return uniquePost;
+//    }
+//
+//    public String getCreatedDate() {
+//        return createdDate;
+//    }
+//
+//    public AuthorDto getAuthor() {
+//        return author;
+//    }
+//
+//    public Boolean getIsVisitorOwner() {
+//        return isVisitorOwner;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public void setBody(String body) {
+//        this.body = body;
+//    }
+//
+//    public void setSelect(String select) {
+//        this.select = select;
+//    }
+//
+//    public void setUniquePost(String uniquePost) {
+//        this.uniquePost = uniquePost;
+//    }
+//
+//    public void setCreatedDate(String createdDate) {
+//        this.createdDate = createdDate;
+//    }
+//
+//    public void setAuthor(AuthorDto author) {
+//        this.author = author;
+//    }
+//
+//    public void setIsVisitorOwner(Boolean visitorOwner) {
+//        isVisitorOwner = visitorOwner;
+//    }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public String getSelect() {
-        return select;
-    }
-
-    public String getUniquePost() {
-        return uniquePost;
-    }
-
-    public String getCreatedDate() {
-        return createdDate;
-    }
-
-    public AuthorDto getAuthor() {
-        return author;
-    }
-
-    public Boolean getIsVisitorOwner() {
-        return isVisitorOwner;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public void setSelect(String select) {
-        this.select = select;
-    }
-
-    public void setUniquePost(String uniquePost) {
-        this.uniquePost = uniquePost;
-    }
-
-    public void setCreatedDate(String createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public void setAuthor(AuthorDto author) {
-        this.author = author;
-    }
-
-    public void setIsVisitorOwner(Boolean visitorOwner) {
-        isVisitorOwner = visitorOwner;
-    }
-
-    @Override
-    public String toString() {
-        return "PostDto{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", body='" + body + '\'' +
-                ", select='" + select + '\'' +
-                ", uniquePost='" + uniquePost + '\'' +
-                ", createdDate='" + createdDate + '\'' +
-                ", author=" + author +
-                ", isVisiterOwner=" + isVisitorOwner +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "PostDto{" +
+//                "id='" + id + '\'' +
+//                ", title='" + title + '\'' +
+//                ", body='" + body + '\'' +
+//                ", select='" + select + '\'' +
+//                ", uniquePost='" + uniquePost + '\'' +
+//                ", createdDate='" + createdDate + '\'' +
+//                ", author=" + author +
+//                ", isVisiterOwner=" + isVisitorOwner +
+//                '}';
+//    }
 }
