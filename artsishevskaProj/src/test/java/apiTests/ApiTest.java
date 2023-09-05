69016d76ec1fa6d0496602e0c04763fec6f92284
MariiaArtsishevska <gloriaaa123321@gmail.com>
2023-09-05 21:30:37 +0300

    ResponseAsString and JsonPath

--- artsishevskaProj/src/test/java/apiTests/ApiTest.java
+++ artsishevskaProj/src/test/java/apiTests/ApiTest.java
@@ -4,38 +4,44 @@ import api.EndPoints;
 import api.dto.responseDto.AuthorDto;
 import api.dto.responseDto.PostDto;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.everyItem;
 
 public class ApiTest {
     final String USER_NAME = "autoapi";
-    Logger logger= Logger.getLogger(getClass());
+    Logger logger = Logger.getLogger(getClass());
+
     @Test
-    public void getPostsByUserTest(){
+    public void getPostsByUserTest() {
         PostDto[] responseAsDto = given()
                 .contentType(ContentType.JSON)//додали хедер аплікейшина
                 .log().all()//виводимо в колсоль весь запит
                 .when()// дія
-                .get(EndPoints.POSTS_BY_USER,USER_NAME )
+                .get(EndPoints.POSTS_BY_USER, USER_NAME)
                 .then()
                 .statusCode(200)
                 .log().all()
                 .assertThat()
                 .body("[0].title", equalTo("test2"))
                 .body("author.username", everyItem(equalTo(USER_NAME)))
-                .extract().body().as(PostDto[].class) ;
+                .extract().body().as(PostDto[].class);
         logger.info(responseAsDto.toString());
         logger.info(responseAsDto.length);
         logger.info(responseAsDto[0].getTitle());
         logger.info(responseAsDto[0].getAuthor().getUsername());
 
-        for (int i = 0; i <responseAsDto.length ; i++) {
-            Assert.assertEquals("UserName is not matched in post" +i,USER_NAME,responseAsDto[i].getAuthor().getUsername());
+        for (int i = 0; i < responseAsDto.length; i++) {
+            Assert.assertEquals("UserName is not matched in post" + i, USER_NAME, responseAsDto[i].getAuthor().getUsername());
 
         }
         PostDto[] expectedPostDto = {
@@ -45,7 +51,7 @@ public class ApiTest {
                         .uniquePost("no").isVisitorOwner(false)
                         .author(AuthorDto.builder().username(USER_NAME).build())
 
-                .build(),
+                        .build(),
                 PostDto.builder().title("test").body("test body").select("All Users")
                         .uniquePost("no").isVisitorOwner(false)
                         .author(AuthorDto.builder().username(USER_NAME).build())
@@ -54,9 +60,8 @@ public class ApiTest {
 
 
         };
-        Assert.assertEquals("Number of posts",expectedPostDto.length,responseAsDto.length);
+        Assert.assertEquals("Number of posts", expectedPostDto.length, responseAsDto.length);
         SoftAssertions softAssertions = new SoftAssertions();
-
 
 
         for (int i = 0; i < expectedPostDto.length; i++) {
@@ -66,7 +71,7 @@ public class ApiTest {
 //                    .isEqualToIgnoringGivenFields(expectedPostDto[i].getAuthor(), "avatar");
             softAssertions.assertThat(responseAsDto)
                     .usingRecursiveComparison()
-                    .ignoringFields("id", "createdDate","author.avatar")
+                    .ignoringFields("id", "createdDate", "author.avatar")
                     .isEqualTo(expectedPostDto);
 
 
@@ -74,4 +79,42 @@ public class ApiTest {
         softAssertions.assertAll();
 
     }
+
+    @Test
+    public void getAllPostByUserNegative() {
+        String actualResponse = given()
+                .contentType(ContentType.JSON)//додали хедер аплікейшина
+                .log().all()//виводимо в колсоль весь запит
+                .when()// дія
+                .get(EndPoints.POSTS_BY_USER, "NotValidUser")
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract().response().body().asString();
+        Assert.assertEquals("Message in response", "\"Sorry, invalid user requested.undefined\"", actualResponse);
+        Assert.assertEquals("Message in response", "Sorry, invalid user requested.undefined", actualResponse.replace("\"", ""));
+    }
+@Test
+    public void getAllPostsByUserPath(){
+    Response response = given()
+            .contentType(ContentType.JSON)//додали хедер аплікейшина
+            .log().all()//виводимо в колсоль весь запит
+            .when()// дія
+            .get(EndPoints.POSTS_BY_USER, USER_NAME)
+            .then()
+            .statusCode(200)
+            .log().all()
+            .extract().response();
+    List<String> actualTitlesList = response.jsonPath().getList("title", String.class);
+    SoftAssertions softAssertions = new SoftAssertions();
+    for (int i = 0; i < actualTitlesList.size(); i++) {
+                softAssertions.assertThat(actualTitlesList.get(i)).as("Item number" + i).contains("test");
+    }
+    List<Map> actualAuthorList = response.jsonPath().getList("author", Map.class);
+    for (int i = 0; i < actualAuthorList.size(); i++) {
+        softAssertions.assertThat(actualAuthorList.get(i).get("username")).as("Item number" + i).isEqualTo(USER_NAME);
+    }
+
+    softAssertions.assertAll();
+}
 }
