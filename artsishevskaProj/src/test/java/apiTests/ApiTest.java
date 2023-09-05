792bb9de246d8afc55687e99d4901dfd6932b790
MariiaArtsishevska <gloriaaa123321@gmail.com>
2023-09-05 22:02:09 +0300

    schema Json

--- artsishevskaProj/src/test/java/apiTests/ApiTest.java
+++ artsishevskaProj/src/test/java/apiTests/ApiTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.everyItem;
 
@@ -117,4 +118,17 @@ public class ApiTest {
 
     softAssertions.assertAll();
 }
+@Test
+    public  void getAllPostsByUserSchema(){
+        given()
+        .contentType(ContentType.JSON)
+            .log().all()
+            .when()
+            .get(EndPoints.POSTS_BY_USER, USER_NAME)
+            .then()
+            .statusCode(200)
+            .log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("response.json"));
+
+    }
 }
