edf1fe9737a3429c023f33cdb95cfa34e8fbf86c
ivan-o-github <127018501+ivan-o-github@users.noreply.github.com>
2023-09-06 06:04:20 +0300

    osipov-05-09-CW-jsonSchema

--- osipovProj/src/test/java/apiTests/ApiTest.java
+++ osipovProj/src/test/java/apiTests/ApiTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.everyItem;
 
@@ -115,5 +116,18 @@ public class ApiTest {
                     .as("Item number " + i).isEqualTo(USER_NAME);
         }
         softAssertions.assertAll();
+    }
+
+    @Test
+    public void getAllPostsByUserSchema(){
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(EndPoints.POSTS_BY_USER, USER_NAME)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("response.json"));
     }
 }
