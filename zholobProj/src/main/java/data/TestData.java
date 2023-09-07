4c49f1667d8341020a3d3f69e1641e8695237a90
zinna2008 <innazaharova1971@gmail.com>
2023-09-07 22:33:41 +0300

    zholob_07_09_api

--- zholobProj/src/main/java/data/TestData.java
+++ zholobProj/src/main/java/data/TestData.java
@@ -1,10 +1,14 @@
 package data;
 
-import libs.ConfigProperties;
 import libs.ConfigProvider;
 
 public class TestData {
     public final static String LOGIN_DEFAULT = System.getProperty("defaultLogin",ConfigProvider.configHiddenProperties.login()); //"qaauto";
     public final static String LOGIN_INVALID = "qaauto1";
     public final static String PASSWORD_DEFAULT = "123456qwerty";
+
+    public final static String LOGIN_API_DEFAULT = "inna";
+    public final static String PASSWORD_API_DEFAULT = "1qaz2wsx3edC+";
+
+
 }
