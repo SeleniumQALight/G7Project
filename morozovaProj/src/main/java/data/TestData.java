7a98a661d721aa5a6cd78c6eb98da820435d1328
MarynaMorozova <marisun.m.2015@gmail.com>
2023-09-07 22:03:11 +0300

    CW-7-09. Create+Delete post API

--- morozovaProj/src/main/java/data/TestData.java
+++ morozovaProj/src/main/java/data/TestData.java
@@ -8,4 +8,7 @@ public class TestData {
     // "qaauto";
     public final static String LOGIN_INVALID = "qaauto1";
     public final static String PASSWORD_DEFAULT = "123456qwerty";
+
+    public final static String LOGIN_API_DEFAULT = "marisun".toLowerCase();
+    public final static String PASSWORD_API_DEFAULT = "123456789012";
 }
