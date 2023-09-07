d7c0350a7eac8985662e6093913fd483174e3690
VladLubynets <hirgoff@gmail.com>
2023-09-07 22:16:00 +0300

    CreatePostByApi

---
+++ lubynetsProj/src/main/java/api/dto/requestDto/CreatePostDto.java
@@ -0,0 +1,19 @@
+package api.dto.requestDto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class CreatePostDto {
+
+    String title;
+    String body;
+    String select1;
+    String uniquePost;
+    String token;
+
+}
