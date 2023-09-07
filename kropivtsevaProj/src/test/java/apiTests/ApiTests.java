add7946305a02845711821d4145d183d2b52e909
Kropivtseva_Daria <kropivtseva.daria@gmail.com>
2023-09-07 12:03:02 +0300

    Schema

--- kropivtsevaProj/src/test/java/apiTests/ApiTests.java
+++ kropivtsevaProj/src/test/java/apiTests/ApiTests.java
@@ -134,5 +134,17 @@ public class ApiTests {
 
         softAssertions.assertAll();
     }
-    
+
+    @Test
+    public void getAllPostByUserSchema() {
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(EndPoints.Posts_by_user, USER_NAME)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("response.json"));
+    }
 }
