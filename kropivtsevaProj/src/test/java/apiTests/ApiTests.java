7bc6723df03fb8d1787bfeca72170e569d4c3c7c
Kropivtseva_Daria <kropivtseva.daria@gmail.com>
2023-09-07 12:02:24 +0300

    respons as String and Map

--- kropivtsevaProj/src/test/java/apiTests/ApiTests.java
+++ kropivtsevaProj/src/test/java/apiTests/ApiTests.java
@@ -4,12 +4,18 @@ import api.EndPoints;
 import api.dto.responseDto.AuthorDto;
 import api.dto.responseDto.PostDto;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.everyItem;
 
@@ -81,4 +87,52 @@ public class ApiTests {
         softAssertions.assertAll();
 
     }
+
+    @Test
+    public void getAllPostByUserNegativeTest() {
+        String actualResponse = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(EndPoints.Posts_by_user, "notValidUser")
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract().response().body().asString();
+        Assert.assertEquals("Massage in response", "\"Sorry, invalid user requested.undefined\"", actualResponse);
+        Assert.assertEquals("Massage in response", "Sorry, invalid user requested.undefined", actualResponse.replace("\"", ""));
+
+    }
+
+    @Test
+    public void getAllPostByUserPath() {
+        Response response =  given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(EndPoints.Posts_by_user, USER_NAME)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract().response();
+
+        List<String> actualTitleList = response.jsonPath().getList("title", String.class);
+        SoftAssertions softAssertions = new SoftAssertions();
+        for (int i = 0; i < actualTitleList.size(); i++) {
+            softAssertions.assertThat(actualTitleList.get(i))
+                    .as("Title is not expected in post " + i)
+                    .contains("test");
+        }
+
+        List<Map> actualAutorList = response.jsonPath().getList("author", Map.class);
+
+        for (int i = 0; i < actualAutorList.size(); i++) {
+            softAssertions.assertThat(actualAutorList.get(i).get("username"))
+                    .as("UserName is not expected in post " + i)
+                    .isEqualTo(USER_NAME);
+        }
+
+        softAssertions.assertAll();
+    }
+    
 }
