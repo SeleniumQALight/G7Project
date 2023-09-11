9cd4bee6de8a3e077fef163e7071caf820d43264
zinna2008 <innazaharova1971@gmail.com>
2023-09-11 22:42:15 +0300

    begin_HW_01_Api_edit

--- zholobProj/src/test/java/privatBankApiTest/PrivatBankApiTest.java
+++ zholobProj/src/test/java/privatBankApiTest/PrivatBankApiTest.java
@@ -4,7 +4,6 @@ import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
-import org.junit.Assert;
 import org.junit.Test;
 import privatBankApi.dto.EndPointsPB;
 import privatBankApi.dto.ExchangeRateDto;
@@ -41,62 +40,68 @@ public class PrivatBankApiTest {
         logger.info(responseAsDto.toString());//
         logger.info(responseAsDto.getExchangeRate().length); // кылькысть елементыв в масиві ExchangeRate
 
-        //       SoftAssertions softAssertions = new SoftAssertions(); // створюємо обєкт класу SoftAssertions для  перевірки
+        SoftAssertions softAssertions = new SoftAssertions(); // створюємо обєкт класу SoftAssertions для  перевірки
 
-        Assert.assertEquals("Date is not matched"
-                , DATE
-                , responseAsDto.getDate());
-        Assert.assertEquals("Bank is not matched"
-                , "PB"
-                , responseAsDto.getBank());
-        Assert.assertEquals("BaseCurrency is not matched"
-                , BASE_CURRENCY
-                , responseAsDto.getBaseCurrency());
-        Assert.assertEquals("BaseCurrencyLit is not matched"
-                , "UAH"
-                , responseAsDto.getBaseCurrencyLit()); // перевіряємо поля date, bank,baseCurrency,baseCurrencyLit
+        softAssertions.assertThat(responseAsDto.getDate())
+                .as("Date is not matched")
+                .isEqualTo(DATE);
 
-        //Всі значення responseAsDto.getExchangeRate()
+        softAssertions.assertThat(responseAsDto.getBank())
+                .as("Bank is not matched")
+                .isEqualTo("PB");
+
+        softAssertions.assertThat(responseAsDto.getBaseCurrency())
+                .as("BaseCurrency is not matched")
+                .isEqualTo(BASE_CURRENCY);
+
+        softAssertions.assertThat(responseAsDto.getBaseCurrencyLit())
+                .as("BaseCurrencyLit is not matched")
+                .isEqualTo("UAH");
+
         for (int i = 0; i < responseAsDto.getExchangeRate().length; i++) {
-            Assert.assertEquals("BaseCurrency is not matched in post  " + i
-                    , "UAH"
-                    , responseAsDto.getExchangeRate()[i].getBaseCurrency());
-
+            softAssertions.assertThat(responseAsDto.getExchangeRate()[i].getBaseCurrency())
+                    .as("BaseCurrency is not matched in post " + i)
+                    .isEqualTo("UAH");
         }
+
+
+
+
 //задаємо очікуваний результат
-        ExchangeRateDto[] expectedExchangeRates = {
-                new ExchangeRateDto("UAH", "AUD"),
-                new ExchangeRateDto("UAH", "AZN"),
-                new ExchangeRateDto("UAH", "BYN"),
-                new ExchangeRateDto("UAH", "CAD"),
-                new ExchangeRateDto("UAH", "CHF"),
-                new ExchangeRateDto("UAH", "CNY"),
-                new ExchangeRateDto("UAH", "CZK"),
-                new ExchangeRateDto("UAH", "DKK"),
-                new ExchangeRateDto("UAH", "EUR"),
-                new ExchangeRateDto("UAH", "GBP"),
-                new ExchangeRateDto("UAH", "GEL"),
-                new ExchangeRateDto("UAH", "HUF"),
-                new ExchangeRateDto("UAH", "ILS"),
-                new ExchangeRateDto("UAH", "JPY"),
-                new ExchangeRateDto("UAH", "KZT"),
-                new ExchangeRateDto("UAH", "MDL"),
-                new ExchangeRateDto("UAH", "NOK"),
-                new ExchangeRateDto("UAH", "PLN"),
-                new ExchangeRateDto("UAH", "SEK"),
-                new ExchangeRateDto("UAH", "SGD"),
-                new ExchangeRateDto("UAH", "TMT"),
-                new ExchangeRateDto("UAH", "TRY"),
-                new ExchangeRateDto("UAH", "UAH"),
-                new ExchangeRateDto("UAH", "USD"),
-                new ExchangeRateDto("UAH", "UZS")
-        };
+    ExchangeRateDto[] expectedExchangeRates = {
+            new ExchangeRateDto("UAH", "AUD"),
+            new ExchangeRateDto("UAH", "AZN"),
+            new ExchangeRateDto("UAH", "BYN"),
+            new ExchangeRateDto("UAH", "CAD"),
+            new ExchangeRateDto("UAH", "CHF"),
+            new ExchangeRateDto("UAH", "CNY"),
+            new ExchangeRateDto("UAH", "CZK"),
+            new ExchangeRateDto("UAH", "DKK"),
+            new ExchangeRateDto("UAH", "EUR"),
+            new ExchangeRateDto("UAH", "GBP"),
+            new ExchangeRateDto("UAH", "GEL"),
+            new ExchangeRateDto("UAH", "HUF"),
+            new ExchangeRateDto("UAH", "ILS"),
+            new ExchangeRateDto("UAH", "JPY"),
+            new ExchangeRateDto("UAH", "KZT"),
+            new ExchangeRateDto("UAH", "MDL"),
+            new ExchangeRateDto("UAH", "NOK"),
+            new ExchangeRateDto("UAH", "PLN"),
+            new ExchangeRateDto("UAH", "SEK"),
+            new ExchangeRateDto("UAH", "SGD"),
+            new ExchangeRateDto("UAH", "TMT"),
+            new ExchangeRateDto("UAH", "TRY"),
+            new ExchangeRateDto("UAH", "UAH"),
+            new ExchangeRateDto("UAH", "USD"),
+            new ExchangeRateDto("UAH", "UZS")
+    };
 
 
 // очікуваний результат
         PrivatCurrencyDto expectedCurrencyDto = new PrivatCurrencyDto(DATE, "PB", BASE_CURRENCY, "UAH", expectedExchangeRates);
 //цикл який перевіряє значення в массиві expectedExchangeRates
-        SoftAssertions softAssertions = new SoftAssertions();
+//        SoftAssertions softAssertions = new SoftAssertions();
+
         //цикл який перевіряє значення в массиві expectedExchangeRates
         for (int i = 0; i < expectedExchangeRates.length; i++) {
             softAssertions.assertThat(responseAsDto.getExchangeRate()[i])
