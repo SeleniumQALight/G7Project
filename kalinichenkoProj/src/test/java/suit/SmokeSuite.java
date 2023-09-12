40f72c6988491265933285e2afaeab559661cd09
mykhailo.kalinichenko <kalinichencomixail@gmail.com>
2023-09-12 20:07:44 +0300

    AllureReport for Api test

--- kalinichenkoProj/src/test/java/suit/SmokeSuite.java
+++ kalinichenkoProj/src/test/java/suit/SmokeSuite.java
@@ -1,5 +1,6 @@
 package suit;
 
+import apiTest.ApiTest;
 import loginTests.LoginTestWithPageObject;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -8,7 +9,8 @@ import postTests.CreatePostWithDB;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         LoginTestWithPageObject.class,
-        CreatePostWithDB.class
+        CreatePostWithDB.class,
+        ApiTest.class
 })
 public class SmokeSuite {
 }
