eec3bef74625d8787a7cc7ff024ca29ba7de9c08
Taras Seliuk <taras.selyuk@gmail.com>
2023-09-12 22:00:13 +0300

    seliuk-12-09-lecture runing the tests via Cucumber

---
+++ seliukProj/src/main/java/bdd/stepDidinitions/LoginStepPages.java
@@ -0,0 +1,18 @@
+package bdd.stepDidinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+
+public class LoginStepPages {
+    @Given("I open login page")
+    public void iOpenLoginPage() {
+        //TODO login page
+    }
+
+    @When("I enter valid cred")
+    public void iEnterValidCred() {
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ seliukProj/src/test/resources/login.feature
@@ -0,0 +1,4 @@
+Feature: Login Feature
+  Scenario: R001 Valid Login
+    Given I open login page
+    When I enter valid cred
