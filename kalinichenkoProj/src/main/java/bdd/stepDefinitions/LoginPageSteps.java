dd84cffb299ee87961aa6057916cea13fe9d9276
mykhailo.kalinichenko <kalinichencomixail@gmail.com>
2023-09-12 21:51:35 +0300

    start BDD add cucumber

---
+++ kalinichenkoProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
@@ -0,0 +1,16 @@
+package bdd.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LoginPageSteps {
+    @Given("I open login page")
+    public void iOpenLoginPage() {
+        //TODO login page open
+    }
+    @When("I enter valid cred")
+    public void i_enter_valid_cred() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
