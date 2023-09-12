88f3c812da822e8a85ecd8fb576060804d3d51f4
Kropivtseva_Daria <kropivtseva.daria@gmail.com>
2023-09-12 18:27:42 +0300

    Create post by API

--- kropivtsevaProj/src/main/java/data/TestData.java
+++ kropivtsevaProj/src/main/java/data/TestData.java
@@ -7,4 +7,7 @@ public class TestData {
     public final static String PASSWORD_DEFOULT = "123456qwerty";
     public final static String LOGIN_invalid = "daria";
     public final static String PASSWORD_invalid = "123456qwert";
+
+    public final static String USER_NAME_API = "dariak".toLowerCase();
+    public final static String PASSWORD_API = "123456qwerty";
 }
