d911c9bb495f8829cefaff716a2523f826fbbb8f
VladLubynets <hirgoff@gmail.com>
2023-09-13 16:21:38 +0300

    login.feature + LoginPageSteps

---
+++ lubynetsProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
@@ -0,0 +1,15 @@
+package bdd.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LoginPageSteps {
+    @Given("I open the login page")
+    public void iOpenTheLoginPage() {
+        //TODO loginPage open
+    }
+    @When("I enter valid credentials")
+    public void i_enter_valid_credentials() {
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ lubynetsProj/src/test/resources/login.feature
@@ -0,0 +1,6 @@
+
+Feature: Login feature
+
+  Scenario: R001 Valid login
+    Given I open the login page
+    When I enter valid credentials
