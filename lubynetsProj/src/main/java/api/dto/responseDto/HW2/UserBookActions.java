bf4bc8c5e54a14c76759da1b32865610d1a72b77
VladLubynets <hirgoff@gmail.com>
2023-09-14 02:01:48 +0300

    HW2 api books fixed

--- lubynetsProj/src/main/java/api/dto/responseDto/HW2/UserBookActions.java
+++ lubynetsProj/src/main/java/api/dto/responseDto/HW2/UserBookActions.java
@@ -10,34 +10,28 @@ import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
 public class UserBookActions {
-    private String token;
-    private String userId;
+    private final String token;
 
-    public UserBookActions(String token, String userId) {
+    public UserBookActions(String token) {
         this.token = token;
-        this.userId = userId;
     }
 
-    public void addBookToUser(String bookISBN) {
-        given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body("{\"userId\":\"" + userId + "\",\"collectionOfIsbns\":[{\"isbn\":\"" + bookISBN + "\"}]}")
-                .when()
-                .post(EndpointHw2.BOOKS);
-    }
-
-    public List<String> getAllBooks() {
-        Response response = given()
+    public List<String> getUserBooks() {
+        Response booksResponse = given()
                 .contentType(ContentType.JSON)
                 .header("Authorization", "Bearer " + token)
                 .when()
                 .get(EndpointHw2.BOOKS);
-        return response.jsonPath().getList("books.isbn");
+
+        assertEquals("Books should be retrieved successfully.", 200, booksResponse.getStatusCode());
+
+        List<String> userBooks = booksResponse.jsonPath().getList("books.isbn");
+
+        return userBooks;
     }
 
     public void verifyUserHasSingleBook(String bookISBN) {
-        List<String> userBooks = getAllBooks();
+        List<String> userBooks = getUserBooks();
         int count = 0;
         for (int i = 0; i < userBooks.size(); i++) {
             if (userBooks.get(i).equals(bookISBN)) {
@@ -45,15 +39,6 @@ public class UserBookActions {
             }
         }
         assertEquals("User should have exactly one book with ISBN: " + bookISBN, 1, count);
-    }
-
-    public void deleteAllBooksForUser() {
-        given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .when()
-                .delete(EndpointHw2.DELETE_BOOKS.replace("{userId}", userId))
-                .then()
-                .statusCode(204);
+        
     }
 }
--- lubynetsProj/src/test/java/apiTests/ApiTestHw2.java
+++ lubynetsProj/src/test/java/apiTests/ApiTestHw2.java
@@ -1,29 +1,92 @@
 package apiTests;
 
+import api.EndpointHw2;
 import api.dto.responseDto.HW2.UserBookActions;
-import api.dto.responseDto.HW2.UserLoginActions;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ApiTestHw2 {
     private String token;
     private String userId;
+    private String firstBookISBN;
+    private UserBookActions userBookActions;
+    Logger logger = Logger.getLogger(ApiTestHw2.class);
 
     @Before
     public void ValidLogin() {
-        UserLoginActions userLogin = new UserLoginActions("vladqaauto", "Vladqaauto7773$");
-        token = userLogin.getToken();
-        userId = userLogin.getUserId();
+        Response loginResponse = given()
+                .contentType(ContentType.JSON)
+                .body("{\"userName\":\"vladqaauto\",\"password\":\"Vladqaauto7773$\"}")
+                .when()
+                .post(EndpointHw2.LOGIN);
+
+        assertEquals("User should be logged in successfully.", 200, loginResponse.getStatusCode());
+
+        token = loginResponse.jsonPath().getString("token");
+        userId = loginResponse.jsonPath().getString("userId");
+        logger.info("User logged in successfully.");
+        userBookActions = new UserBookActions(token);
+
     }
 
     @Test
     public void testBookstoreActions() {
-        UserBookActions userBookActions = new UserBookActions(token, userId);
-        String bookISBN = "9781449325862";
-        userBookActions.deleteAllBooksForUser();
-        userBookActions.getAllBooks();
-        userBookActions.addBookToUser(bookISBN);
-        userBookActions.verifyUserHasSingleBook(bookISBN);
+        assertTrue("Token should not be empty.", !token.isEmpty());
+        assertTrue("UserId should not be empty.", !userId.isEmpty());
+        logger.info("Token and UserId are not empty.");
+
+        Response deleteResponse = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + token)
+                .queryParam("UserId", userId)
+                .delete(EndpointHw2.DELETE_BOOKS);
+
+        assertEquals("All user's books should be deleted.", 204, deleteResponse.getStatusCode());
+        logger.info("All user's books have been deleted.");
+
+        Response booksResponse = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get(EndpointHw2.BOOKS);
+
+        assertEquals("Books should be retrieved successfully.", 200, booksResponse.getStatusCode());
+        firstBookISBN = booksResponse.jsonPath().getString("books[0].isbn");
+        logger.info("Retrieved the first book ISBN: " + firstBookISBN);
+
+
+        Response addBookResponse = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + token)
+                .body("{\"userId\":\"" + userId + "\",\"collectionOfIsbns\":[{\"isbn\":\"" + firstBookISBN + "\"}]}")
+                .when()
+                .post(EndpointHw2.BOOKS);
+
+        assertEquals("Book should be added successfully to the user.", 201, addBookResponse.getStatusCode());
+        logger.info("Book has been added to the user");
+
+        userBookActions.verifyUserHasSingleBook(firstBookISBN);
+        logger.info("User has only one book with ISBN: " + firstBookISBN);
+    }
+
+    @After
+    public void deleteAllUserBooks() {
+        Response deleteResponse = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + token)
+                .queryParam("UserId", userId)
+                .delete(EndpointHw2.DELETE_BOOKS);
+
+        assertEquals("All user's books should be deleted.", 204, deleteResponse.getStatusCode());
+        logger.info("All user's books have been deleted.");
     }
 
 }
