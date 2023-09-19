a47c307789fbafe72cee29d9ec68a08e7ec55c1e
VladLubynets <hirgoff@gmail.com>
2023-09-19 21:01:53 +0300

    Valid Test login.feature

--- lubynetsProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
+++ lubynetsProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
@@ -1,15 +1,23 @@
 package bdd.stepDefinitions;
 
+import bdd.helpers.WebDriverHelper;
+import data.TestData;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
-public class LoginPageSteps {
+public class LoginPageSteps  extends MainSteps{
+    public LoginPageSteps(WebDriverHelper webDriverHelper) {
+        super(webDriverHelper);
+    }
+
     @Given("I open the login page")
     public void iOpenTheLoginPage() {
-        //TODO loginPage open
+        pageProvider.getLoginPage().openLoginPage();
     }
-    @When("I enter valid credentials")
+    @When("I login with valid credentials")
     public void i_enter_valid_credentials() {
-        throw new io.cucumber.java.PendingException();
+    pageProvider.getLoginPage().enterTextIntoInputUserName(TestData.LOGIN_DEFAULT1);
+    pageProvider.getLoginPage().enterTextIntoInputPassword(TestData.PASSWORD_DEFAULT);
+    pageProvider.getLoginPage().clickOnButtonSignIn();
     }
 }
--- lubynetsProj/src/test/resources/login.feature
+++ lubynetsProj/src/test/resources/login.feature
@@ -1,6 +1,8 @@
-
+@LoginFeature @Regression
 Feature: Login feature
 
+  @R001 @Smoke
   Scenario: R001 Valid login
     Given I open the login page
-    When I enter valid credentials
+    When I login with valid credentials
+    Then I see avatar on Home page
