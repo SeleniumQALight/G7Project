41548d19f56c156995683b397be8f96e5152d9aa
zinna2008 <innazaharova1971@gmail.com>
2023-09-19 19:06:10 +0300

    cucumber_01

---
+++ zholobProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
@@ -0,0 +1,17 @@
+package bdd.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LoginPageSteps {
+    @Given("I open Login page")
+    public void iOpenLoginPage() {
+        // TODO loginPage open
+    }
+
+    @When("I enter valid cred")
+    public void i_enter_valid_cred() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
