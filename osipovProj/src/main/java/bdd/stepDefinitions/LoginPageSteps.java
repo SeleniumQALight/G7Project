6921e3fb2233b114a6d06cc99ae53d08cc3fc7f7
ivan-o-github <127018501+ivan-o-github@users.noreply.github.com>
2023-09-19 21:39:49 +0300

    cucumber-pt3-test R002 was finished

--- osipovProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
+++ osipovProj/src/main/java/bdd/stepDefinitions/LoginPageSteps.java
@@ -3,6 +3,7 @@ package bdd.stepDefinitions;
 import bdd.helpers.WebDriverHelper;
 import data.TestData;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginPageSteps extends MainSteps{
@@ -20,5 +21,21 @@ public class LoginPageSteps extends MainSteps{
         pageProvider.getLoginPage().enterTextIntoInputUserName(TestData.LOGIN_DEFAULT);
         pageProvider.getLoginPage().enterTextIntoInputPassword(TestData.PASSWORD_DEFAULT);
         pageProvider.getLoginPage().clickOnButtonSignIn();
+    }
+    @When("I enter {string} into input Login in Login form")
+    public void i_enter_into_input_login_in_login_form(String username) {
+        pageProvider.getLoginPage().enterTextIntoInputUserName(username);
+    }
+    @When("I enter {string} into input Password in Login form")
+    public void i_enter_into_input_password_in_login_form(String password) {
+        pageProvider.getLoginPage().enterTextIntoInputPassword(password);
+    }
+    @When("I click on button SignIn in Login form")
+    public void i_click_on_button_sign_in_in_login_form() {
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+    }
+    @Then("I see alert message text with text {string}")
+    public void i_see_alert_message_text_with_text(String textMessage) {
+        pageProvider.getLoginPage().checkIsErrorMessageVisible(textMessage);
     }
 }
