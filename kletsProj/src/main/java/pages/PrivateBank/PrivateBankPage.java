a2760d0fa09403a61f133c9599bfe08e4610bfa4
Natali <klets.natali@gmail.com>
2023-10-08 09:42:02 -0700

    extra test and file were deleted Assert was added

--- kletsProj/src/main/java/pages/PrivateBank/PrivateBankPage.java
+++ kletsProj/src/main/java/pages/PrivateBank/PrivateBankPage.java
@@ -2,6 +2,7 @@ package pages.PrivateBank;
 
 import data.TestData;
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,7 +10,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-
 
 
 public class PrivateBankPage extends ParentPagePB {
@@ -53,15 +53,11 @@ public class PrivateBankPage extends ParentPagePB {
 
 
     public void compareRates() {
-        if (TestData.rate_buy_api.equals(TestData.rate_buy_ui)) {
-            logger.info("Buy rate from API and UI are equal");
-        } else {
-            logger.info("Buy rate from API and UI are NOT equal");
-        }
-        if (TestData.rate_sell_api.equals(TestData.rate_sell_ui)) {
-            logger.info("Sell rate from API and UI are equal");
-        } else {
-            logger.info("Sell rate from API and UI are NOT equal");
-        }
+        Assert.assertEquals("Buy rate from UI is not equal to Buy rate from API", TestData.rate_buy_ui, TestData.rate_buy_api);
+        logger.info("Buy rate from UI is equal to Buy rate from API");
+        Assert.assertEquals("Sell rate from UI is not equal to Buy rate from API", TestData.rate_sell_ui, TestData.rate_sell_api);
+        logger.info("Sell rate from UI is equal to Buy rate from API");
     }
+
+
 }
