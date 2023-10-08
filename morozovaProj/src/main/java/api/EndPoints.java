b34275f4bac5614f7838f06fa0998607966ae358
MarynaMorozova <marisun.m.2015@gmail.com>
2023-10-08 16:59:05 +0300

    Exam Cucumber fix url

--- morozovaProj/src/main/java/api/EndPoints.java
+++ morozovaProj/src/main/java/api/EndPoints.java
@@ -1,23 +1,18 @@
 package api;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-
 public interface EndPoints {
-        final String BASE_URL = "https://aqa-complexapp.onrender.com";
-        final String POSTS_BY_USER = BASE_URL + "/api/postsByAuthor/{0}";//ресташуред {0} працює аналогічно стрінгформату
-        final String LOGIN = BASE_URL + "/api/login";
-        final String CREATE_POST = BASE_URL + "/api/create-post";
-        final String DELETE_POST = BASE_URL + "/api/post/{0}";
+    final String BASE_URL = "https://aqa-complexapp.onrender.com";
+    final String POSTS_BY_USER = BASE_URL + "/api/postsByAuthor/{0}";//ресташуред {0} працює аналогічно стрінгформату
+    final String LOGIN = BASE_URL + "/api/login";
+    final String CREATE_POST = BASE_URL + "/api/create-post";
+    final String DELETE_POST = BASE_URL + "/api/post/{0}";
 
-        //    PrivatBankEndPoints
-        final String PRIVATBANK_UIURL = "https://privatbank.ua";
+    //    PrivatBankEndPoints
+    final String PRIVATBANK_UIURL = "https://privatbank.ua";// UI
 
-        final String BASE_URL_PRIVAT = "https://api.privatbank.ua/p24api";
-        final String PRIVAT_URL = BASE_URL_PRIVAT + "/exchange_rates";
-        //  https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5 - API get currency
-        //String queryParams = "json&exchange&coursid=5";
-        //final String PRIVATBANK_URL = BASE_URL_PRIVAT +"/pubinfo?" + queryParams;
-    final String PRIVATBANK_URL = BASE_URL_PRIVAT +"/pubinfo"; //+ queryParams;
-    }
+    final String BASE_URL_PRIVAT = "https://api.privatbank.ua/p24api";// тут з апі
+   // GET https://api.privatbank.ua/p24api/exchange_rates
+    final String PRIVAT_URL = BASE_URL_PRIVAT + "/exchange_rates";
+    //  https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5 - API get currency
+    final String PRIVATBANK_URL = BASE_URL_PRIVAT + "/pubinfo";
+}
