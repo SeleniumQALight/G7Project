0caf50330aa5c3a1f0fa11d8795c54dd8f778586
zinna2008 <innazaharova1971@gmail.com>
2023-10-13 02:29:39 +0300

    Zholob_HW_2_API

---
+++ zholobProj/src/test/java/BooksDemoqa/BooksDto/CollectionOfIsbnsDto.java
@@ -0,0 +1,16 @@
+package BooksDemoqa.BooksDto;
+
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+
+public class CollectionOfIsbnsDto {
+    String isbn;
+}
+
---
+++ zholobProj/src/test/java/BooksDemoqa/TestDataBooks.java
@@ -0,0 +1,6 @@
+package BooksDemoqa;
+
+public class TestDataBooks {
+    public final static String DEFAULT_USERNAME = "inna";
+    public final static String DEFAULT_PASSWORD = "Nhfvgfvgfv_369*";
+}
